1617b6ea68ec0c8336e4447e58195d46b24421b1
Vikas <kedia.vikas@gmail.com>
2020-07-26 15:47:23 -0700

    finding the user on github runner

--- .github/run-github-actions-locally.sh
+++ .github/run-github-actions-locally.sh
@@ -9,14 +9,11 @@ docker rm $(docker ps -a -q)
 # remove all docker images
 # docker rmi $(docker images -q)
 
-echo "Step 3/6 Deleting the mysql data dir so previous run does not affect this run"
-rm -rf /tmp/var/
-
-echo "Step 4/6 Installing app from https://github.com/nektos/act"
+echo "Step 3/5 Installing app from https://github.com/nektos/act"
 brew install nektos/tap/act
 
-echo "Step 5/6  Download the full ubuntu image to make env similar to github https://github.com/nektos/act#alternative-runner-images"
+echo "Step 4/5  Download the full ubuntu image to make env similar to github https://github.com/nektos/act#alternative-runner-images"
 docker run --rm -it --entrypoint "" nektos/act-environments-ubuntu:18.04-full ls
 
-echo "Step 6/6 This will automatically pick up workflows/build.yml"
+echo "Step 5/5 This will automatically pick up workflows/build.yml"
 act -P ubuntu-18.04=nektos/act-environments-ubuntu:18.04-full
