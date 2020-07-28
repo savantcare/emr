2391e5a293d69bb0082319f3d1bdc22b5f41ac30
Vikas <kedia.vikas@gmail.com>
2020-07-28 08:45:26 -0700

    updated doc

--- ptclient/cts/core/manage-csvl-cards/orm-search-phrases-of-ct.js
+++ ptclient/cts/core/manage-csvl-cards/orm-search-phrases-of-ct.js
@@ -9,7 +9,7 @@ export default class ormSearchPhrasesOfCt extends Model {
       value: this.string(''), // this is the search term in the dropdown of the autcocomplete in view layer and change layer. I call it value since autocomplete function expects the object property to be called value
       ctAbbr: this.string(''), // These are industry terms like social history is called shx in medical industry
       ctToShow: this.string(''), // ctToShow => ct To Show
-      location: this.string(''), // This can have the value of "change" or "view"
+      location: this.string(''), // This can have the value of "csvl" or "vl"
       usageCountKeptInOrm: this.number(0), // usageCountKeptInOrm => usage Count Kept In Local storate. This is used to order the search results.
       ctWidth: this.string(''), // In CL we want to box to be as small as possible to more area of Vl is visible.
     }
