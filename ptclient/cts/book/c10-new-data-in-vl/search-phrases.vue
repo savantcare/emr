bdfeb671ccbd23a909033d657da57132a842e5be
Vikas <kedia.vikas@gmail.com>
2020-07-28 06:18:40 -0700

    started C10

---
+++ ptclient/cts/book/c10-new-data-in-vl/search-phrases.vue
@@ -0,0 +1,28 @@
+<template> <div></div> </template>
+<script>
+import ormSearchPhrasesOfCt from '@/cts/core/manage-csvl-cards/orm-search-phrases-of-ct'
+export default {
+  data() {
+    return {}
+  },
+  mounted() {
+    ormSearchPhrasesOfCt.insert({
+      data: {
+        value: 'Ninth chapter',
+        ctAbbr: 'c9',
+        ctToShow: 'book/c9-remove-data-rows/vl/table.vue', // CsVl -> Current state view layer
+        location: 'csvl',
+      },
+    })
+    ormSearchPhrasesOfCt.insert({
+      data: {
+        value: 'Ninth chapter - change',
+        ctAbbr: 'c9-c',
+        ctToShow: 'book/c9-remove-data-rows/cl/table.vue',
+        location: 'cl',
+        ctWidth: 'small',
+      },
+    })
+  },
+}
+</script>
---
+++ ptclient/cts/book/c10-new-data-in-vl/vl/table.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    From Ninth chapter - Hello {{ cfMsg }}
+    <el-button style="padding: 3px;" type="success" plain @click="mfOpenCtInCl">A</el-button>
+  </div>
+</template>
+<script>
+import ormHelloWorld from '@/cts/book/c7-multiple-data-rows/db/vuex-orm/helloworld.js'
+export default {
+  computed: {
+    cfMsg() {
+      const arFromORM = ormHelloWorld.query().get()
+      if (arFromORM.length > 0) {
+        console.log(arFromORM)
+        return arFromORM[0].msg
+      }
+      return ''
+    },
+  },
+  mounted() {
+    ormHelloWorld.insert({ data: { id: 1, msg: 'John' } })
+    console.log(ormHelloWorld)
+  },
+  methods: {
+    mfOpenCtInCl() {
+      this.$store.commit('mtfShowNewFirstTabInClFromSearchPhrase', {
+        searchTerm: 'Eight chapter - change',
+      })
+    },
+  },
+}
+</script>
