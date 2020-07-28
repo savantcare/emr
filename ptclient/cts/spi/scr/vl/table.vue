16e25a37d6101a0e4d1330879a20604a266930e2
tanmoyjana-grmtech <tanmoyjana@grmtech.com>
2020-07-28 13:10:39 +0530

    initialized screening component

---
+++ ptclient/cts/spi/scr/vl/table.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>Screening</span>
+        <el-button-group style="float: right;">
+          <el-button style="padding: 3px;" type="success" plain tabindex="-1">A</el-button>
+          <el-button style="padding: 3px;" type="primary" plain tabindex="-1">G</el-button>
+        </el-button-group>
+      </div>
+      <div>In progress...</div>
+    </el-card>
+  </div>
+</template>
