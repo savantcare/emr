f5d0f258e27bf2066cf09e87edeeefcf522bee04
Vikas <kedia.vikas@gmail.com>
2020-08-02 22:42:51 -0700

    more readable

--- ptclient/cts/spi/1rNf/name/db/full-sync-with-server-db-mixin.js
+++ ptclient/cts/spi/1rNf/name/db/full-sync-with-server-db-mixin.js
@@ -2,8 +2,7 @@ import ormName from '~/cts/spi/1rNf/name/db/orm-name.js'
 export default {
   methods: {
     async getDataFromDBMx() {
-      // check length and only load if its empty
-      // Also need to restrict the load to current patient
+      // TODO: Load based on patient uuid in the URL
       const proNameFromDB = await ormName.api().get(ormName.apiUrl + '/1')
       if (proNameFromDB.ok) {
       }
