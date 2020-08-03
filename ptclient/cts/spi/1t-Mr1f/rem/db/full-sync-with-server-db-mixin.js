568dbdc1b46bf59f8c98a1206d485068a7202e64
Vikas <kedia.vikas@gmail.com>
2020-08-03 00:04:45 -0700

    updated

--- ptclient/cts/spi/1t-Mr1f/rem/db/full-sync-with-server-db-mixin.js
+++ ptclient/cts/spi/1t-Mr1f/rem/db/full-sync-with-server-db-mixin.js
@@ -3,6 +3,7 @@ export default {
   methods: {
     async getDataFromDBMx() {
       // Need to restrict the load to current patient
+      // api is vuex-orm-axios plugin function
       const proRemsFromDB = await ormRem.api().get(ormRem.apiUrl + '/getAll')
       if (proRemsFromDB.ok) {
       }
