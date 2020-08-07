b2c7e9b6a76c55cf4003b69b5c72eeceffaf208e
Vikas <kedia.vikas@gmail.com>
2020-08-06 20:50:27 -0700

    better doc

--- ptclient/cts/spi/1t-1rMf/name/cl/c.vue
+++ ptclient/cts/spi/1t-1rMf/name/cl/c.vue
@@ -38,12 +38,11 @@ import mxFullSyncWithDbServer from '../db/full-sync-with-db-server-mixin'
 export default {
   mixins: [mxFullSyncWithDbServer],
 
-  /*  if the name was changed 4 times earlier so the id will not be 1 it will be 5. The id will always be the highest row that is not valid and ends in 1
-      I want doctor to be able to type: "change name" though this operates on a row there is only one row.
-      When doctor types "change name" the id cannot be passed to this ct. This ct needs to find this id on its own. */
   data() {
     return {
-      /* 1. row to change is not same as row being changed. This row is the latest data where ROW_END is in future and row status ends in 1   */
+      /*  if the name was changed 4 times before this ct loaded the id will be 5. The id will always be latest data where ROW_END is in future and row status ends in 1
+          I want doctor to be able to type: "change name" though this operates on a row there is only one row.
+          When doctor types "change name" the id cannot be passed to this ct. This ct needs to find this id on its own. */
       vnOrmIdOfRowToChange: null,
 
       /* Why not change the original row? 1. If the user hits reset I cannot go back to the data that the user started with. 2. Server side is temporal DB where the origianl data row is not changed. Only ROW_START and ROW_END are changed. */
@@ -52,7 +51,9 @@ export default {
   },
   computed: {
     cfHasSomeFieldChanged() {
-      if (this.vnOrmIdOfCopiedRowBeingChanged === -1) return true // there is a race condition. This if statement waits for copy to finish
+      // Following 2 protect against race condition. Since data needs to be loaded inside created function and then the row needs to get copied
+      // When the Ct is created vnOrmIdOfRowToChange and vnOrmIdOfCopiedRowBeingChanged are -1 indicating they are undefined. They need to have a value before this code should execute
+      if (this.vnOrmIdOfCopiedRowBeingChanged === -1) return true
       if (this.vnOrmIdOfCopiedRowBeingChanged === null) return true
 
       /* Why do I need to have the fields that are being changed? Why not just use the rowStatus field to decide if the row has changed? Better Ux
