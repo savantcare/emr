43cac252ca13ac584caf6793e79eea82493e983e
Vikas <kedia.vikas@gmail.com>
2020-08-09 01:55:52 -0700

    in BM subtabs height graph comes

--- ptclient/cts/spi/1t-1rMf/bm/vl/tabs.vue
+++ ptclient/cts/spi/1t-1rMf/bm/vl/tabs.vue
@@ -3,7 +3,7 @@
     <h5>Body measurement</h5>
     <el-tabs tab-position="left" type="card">
       <el-tab-pane label="Wt"> <ctWeightGraph form-type="embedded"></ctWeightGraph></el-tab-pane>
-      <el-tab-pane label="Ht"> <ctHeightTable form-type="embedded"></ctHeightTable></el-tab-pane>
+      <el-tab-pane label="Ht"> <ctHeightGraph form-type="embedded"></ctHeightGraph></el-tab-pane>
       <el-tab-pane label="Tm">
         <ctTemperatureTable form-type="embedded"></ctTemperatureTable>
       </el-tab-pane>
@@ -13,10 +13,9 @@
 
 <script>
 import ctWeightGraph from '@/cts/spi/1t-1rMf/weight/vl/line-graph.vue'
-// import ctWeightTable from '@/cts/spi/1t-1rMf/weight/vl/table.vue'
-import ctHeightTable from '@/cts/spi/1t-1rMf/height/vl/table.vue'
+import ctHeightGraph from '@/cts/spi/1t-1rMf/height/vl/line-graph.vue'
 import ctTemperatureTable from '@/cts/spi/1t-1rMf/temperature/vl/table.vue'
 export default {
-  components: { ctWeightGraph, ctHeightTable, ctTemperatureTable },
+  components: { ctWeightGraph, ctHeightGraph, ctTemperatureTable },
 }
 </script>
