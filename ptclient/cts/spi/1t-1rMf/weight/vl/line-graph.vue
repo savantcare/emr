5d282c822c3f3f298efea8f99385d1f6e9241372
Vikas <kedia.vikas@gmail.com>
2020-08-09 01:33:23 -0700

    chartoptions for graph comes from mixin

--- ptclient/cts/spi/1t-1rMf/weight/vl/line-graph.vue
+++ ptclient/cts/spi/1t-1rMf/weight/vl/line-graph.vue
@@ -38,72 +38,17 @@
 import { Chart } from 'highcharts-vue'
 import mxTable from '../table-mixin.js'
 import orm from '../db/orm.js'
+import mxLineG from '../line-graph-mixin.js'
 
 export default {
   components: {
     highcharts: Chart,
   },
-  mixins: [mxTable],
+  mixins: [mxTable, mxLineG],
   props: {
     formType: {
       default: 'stand-alone',
       type: String,
-    },
-  },
-  computed: {
-    chartOptions() {
-      const arDataToShowOnGraph = this.mfGetDataForGraph()
-
-      // if (!arDataToShowOnGraph) return {} // In this case the tooltip will not come property formatted. Since during 2nd run it is updating the data but not some of the other chartOptions
-
-      console.log(arDataToShowOnGraph)
-
-      // Ref: https://www.tutorialspoint.com/highcharts/highcharts_spline_time.htm
-      const chartOptions = {
-        series: [
-          {
-            name: orm.entity,
-            showInLegend: false,
-            data: arDataToShowOnGraph,
-          },
-        ],
-        title: {
-          text: '',
-        }, // Reason: Y axis will have orm.entity for e.g. "weight" written beside it. This is small space. Difficult design decisions need to be made instead of doing everything.
-        xAxis: [
-          {
-            title: {
-              text: 'Measurement date',
-            },
-            type: 'datetime',
-            labels: {
-              enabled: 'true',
-              format: '{value:%m-%Y}', // X axis now shows month and year
-            },
-          },
-        ],
-        yAxis: [
-          {
-            title: {
-              text: orm.entity,
-            },
-          },
-        ],
-        tooltip: {
-          headerFormat: '<b>{point.y:.2f} lbs</b><br>',
-          pointFormat: '{point.x:%m-%Y}',
-          /* TODO: 
-          1. Instead of month number need to show month short form. 
-          2. In graph sometimes the tooltip format is ok and other times not. Seems like a race condition. 
-          3. json-server needs to send only 1 valid and other discontinued data.
-          4. On changing the data the json-server data insert and update of old data needs to work. In mariaDB this is transparent. Should I use to MaroaDB now? 
-          5. Show notes when click on popup */
-        },
-        credits: {
-          enabled: false,
-        },
-      } // finished defining chartOptions
-      return chartOptions
     },
   },
   methods: {
