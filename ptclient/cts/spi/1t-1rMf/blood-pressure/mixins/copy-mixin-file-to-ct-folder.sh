6ab9b4107eabe57e753102ab110b3c918d072761
Vikas <kedia.vikas@gmail.com>
2020-08-15 01:20:59 -0700

    BP and BS works

---
+++ ptclient/cts/spi/1t-1rMf/blood-pressure/mixins/copy-mixin-file-to-ct-folder.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir -p ../../blood-pressure/mixins/
+cp * ../../blood-pressure/mixins/ 
+
+mkdir -p ../../blood-sugar/mixins/
+cp * ../../blood-sugar/mixins/
+
+mkdir -p ../../date-of-birth/mixins/
+cp * ../../date-of-birth/mixins/
+
+mkdir -p ../../height/mixins/ 
+cp * ../../height/mixins/
+
+mkdir -p ../../oxygen-saturation/mixins/
+cp * ../../oxygen-saturation/mixins/
+
+mkdir -p ../../pulse/mixins/
+cp * ../../pulse/mixins/ 
+
+mkdir -p ../../temperature/mixins/
+cp * ../../temperature/mixins/ 
+
+mkdir -p ../../waist-circumference/mixins/
+cp * ../../waist-circumference/mixins/ 
+
+# mkdir -p ../../weight/mixins/ 
+# cp * ../../weight/mixins/ 
