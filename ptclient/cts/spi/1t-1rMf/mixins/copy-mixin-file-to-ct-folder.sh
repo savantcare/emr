a04616342fe9c0de45134a00d91af2171ff2d224
Vikas <kedia.vikas@gmail.com>
2020-08-14 23:55:33 -0700

    shell script

---
+++ ptclient/cts/spi/1t-1rMf/mixins/copy-mixin-file-to-ct-folder.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp mixin-line-graph.js mixin-change-layer.js mixin-view-layer.js ../date-of-birth/
+cp mixin-line-graph.js mixin-change-layer.js mixin-view-layer.js ../weight/ 
+cp mixin-line-graph.js mixin-change-layer.js mixin-view-layer.js ../height/ 
+cp mixin-line-graph.js mixin-change-layer.js mixin-view-layer.js ../blood-pressure/ 
+cp mixin-line-graph.js mixin-change-layer.js mixin-view-layer.js ../blood-sugar/
