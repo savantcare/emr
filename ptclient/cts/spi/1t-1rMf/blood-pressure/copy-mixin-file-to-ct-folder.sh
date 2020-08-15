b4e1f2af9fa5e088a969570fad06520b8b88874f
Vikas <kedia.vikas@gmail.com>
2020-08-15 01:09:18 -0700

    improved

---
+++ ptclient/cts/spi/1t-1rMf/blood-pressure/copy-mixin-file-to-ct-folder.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+mkdir -p ../../date-of-birth/mixins/
+cp * ../../date-of-birth/mixins/
+
+mkdir -p ../../height/ 
+cp * ../../height/ 
+
+mkdir -p ../../oxygen-saturation/mixins/
+cp * ../../oxygen-saturation/mixins/
+
+mkdir -p ../../pulse/ 
+cp * ../../pulse/ 
+
+mkdir -p ../../temperature/ 
+cp * ../../temperature/ 
+
+mkdir -p ../../waist-circumference/
+cp * ../../waist-circumference/ 
+
+mkdir -p ../../blood-pressure/
+cp * ../../blood-pressure/ 
+
+mkdir -p ../../blood-sugar/
+cp * ../../blood-sugar/
+
+# mkdir -p ../../weight/mixins/ 
+# cp * ../../weight/mixins/ 
