485f613a27c4483c88e2c67ce6ee43ef035ea4ed
Vikas <kedia.vikas@gmail.com>
2020-08-15 02:26:49 -0700

    + read

--- ptclient/cts/core/mtsvl-header/slider.vue
+++ ptclient/cts/core/mtsvl-header/slider.vue
@@ -1,7 +1,3 @@
-/* Single instance component names Components that should only ever have a single active instance
-should begin with the The prefix, to denote that there can be only one. Ref:
-https://vuejs.org/v2/style-guide/#Single-instance-component-names-strongly-recommended */
-
 <template>
   <vue-slider
     v-model="sliderCurrentValue"
@@ -234,11 +230,3 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-#TheMultiStateDisplayAreaHeader {
-  height: 30px;
-  margin-bottom: 6px;
-  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
-}
-</style>
