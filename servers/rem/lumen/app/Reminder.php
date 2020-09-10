9e0ef5156cead06dcc93d20fd529be8002d40590
raj.roushan <raj@grmtech.com>
2020-09-10 21:29:42 +0530

    Now rem fetch, add, change and discontinue api run from lumen(laravel)

--- servers/rem/lumen/app/Reminder.php
+++ servers/rem/lumen/app/Reminder.php
@@ -3,10 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
 
 class Reminder extends Model
 {
     protected $table = 'rems';
+    public $timestamps = false;
+    protected $primaryKey = 'uuid';
+    protected $keyType = 'string';
+    public $incrementing = false;
 
     /**
      * The attributes that are mass assignable.
@@ -14,7 +19,7 @@ class Reminder extends Model
      * @var array
      */
     protected $fillable = [
-        'description', 'notes'
+        'ptUUID', 'description', 'notes', 'recordChangedByUUID'
     ];
 
     /**
@@ -23,4 +28,14 @@ class Reminder extends Model
      * @var array
      */
     protected $hidden = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function (Model $model) {
+            $model->setAttribute($model->getKeyName(), Uuid::uuid4());
+            // $model->ROW_START = '2038-10-10 11:11:11';
+        });
+    }
 }
