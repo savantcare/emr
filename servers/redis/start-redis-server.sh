6314d924514e76aad6fa707d4778812807a4fb32
Vikas <kedia.vikas@gmail.com>
2020-09-11 00:42:54 -0700

    mr

--- servers/redis/start-redis-server.sh
+++ servers/redis/start-redis-server.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+echo "For this script to run redis-server must be installed"
+echo "On mac the command I gave was > brew install redis"
+echom "On windows download binary from https://github.com/dmajkic/redis/downloads"
 redis-server
