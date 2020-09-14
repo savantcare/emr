f6cf7b02d7072f8d64b50566f7fe759a13578572
Vikas <kedia.vikas@gmail.com>
2020-09-13 23:34:08 -0700

    mr

--- servers/redis/start-redis-web-interface.sh
+++ servers/redis/start-redis-web-interface.sh
@@ -1,6 +1,6 @@
- #!/bin/bash
+#!/bin/bash
 
- echo "Redis commander needs to be installed"
- echo "For mac the command is npm install -g redis-commander"
+echo "Redis commander needs to be installed"
+echo "For mac the command is npm install -g redis-commander"
  
- redis-commander # this listens on port 8081
+redis-commander # this listens on port 8081
