2f370075f650d3a1ec0aad5c05fdcb8895248aec
Vikas <kedia.vikas@gmail.com>
2020-09-17 22:22:54 -0700

    mr

--- webclient/store/import-models-and-register-to-db-of-vuex-orm.js
+++ webclient/store/import-models-and-register-to-db-of-vuex-orm.js
@@ -1,50 +1,50 @@
 // this is file 3 in example
 
 import { Database } from '@vuex-orm/core'
-import CtSearch from '~/components/core/search-phrases/search-phrases-of-components-table'
-import CtLifeCycle from '~/components/core/ctMaster/orm-ct-life-cycle'
-import Rem from '~/components/pt-info/single/1t-Mrow-1Field/rem/db/vuex-orm/rem-table.js'
-import ormName from '~/components/pt-info/single/1t-1row-mField/name/db/table.js'
-import ormWeight from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/weight/db/table.js'
-import ormWaistCircumference from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/waist-circumference/db/table.js'
-import ormBloodPressure from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/blood-pressure/db/table.js'
-import ormBloodSugar from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/blood-sugar/db/table.js'
-import ormOxygenSaturation from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/oxygen-saturation/db/table.js'
-import ormHeight from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/height/db/table.js'
-import ormTemperature from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/temperature/db/table.js'
-import ormPulse from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/pulse/db/table.js'
-import ormDob from '~/components/pt-info/single/1t-1row-mField/date-of-birth/db/table.js'
-import ormPhq9 from '~/components/pt-info/single/1t-1row-mField/phq9/db/table.js'
-import Rec from '~/components/pt-info/single/1t-Mrow-1Field/rec/db/vuex-orm/model.js'
-import Dx from '~/components/pt-info/single/1t-Mrow-mField/dx/db/vuex-orm/Dx.js'
-import Goal from '~/components/pt-info/single/1t-Mrow-mField/goal/db/vuex-orm/Goal'
-import Scr from '~/components/pt-info/single/1t-Mrow-mField/scr/db/vuex-orm/Screening'
-import Dxa from '~/components/pt-info/single/1t-Mrow-mField/dx/db/vuex-orm/Dxa.js'
-import phoneNumbers from '~/components/pt-info/single/1t-Mrow-mField/phone-numbers/db/vuex-orm/table.js'
-import objOrm from '~/components/pt-info/single/1t-1row-mField/common-for-all-components/db/table.js'
+import tableSearch from '~/components/core/search-phrases/search-phrases-of-components-table'
+import tableLifeCycle from '~/components/core/ctMaster/orm-ct-life-cycle'
+import tableRem from '~/components/pt-info/single/1t-Mrow-1Field/rem/db/vuex-orm/rem-table.js'
+import tableName from '~/components/pt-info/single/1t-1row-mField/name/db/table.js'
+import tableWeight from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/weight/db/table.js'
+import tableWaistCircumference from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/waist-circumference/db/table.js'
+import tableBloodPressure from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/blood-pressure/db/table.js'
+import tableBloodSugar from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/blood-sugar/db/table.js'
+import tableOxygenSaturation from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/oxygen-saturation/db/table.js'
+import tableHeight from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/height/db/table.js'
+import tableTemperature from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/temperature/db/table.js'
+import tablePulse from '~/components/pt-info/single/1t-1row-mField/bm/sub-cts/pulse/db/table.js'
+import tableDob from '~/components/pt-info/single/1t-1row-mField/date-of-birth/db/table.js'
+import tablePhq9 from '~/components/pt-info/single/1t-1row-mField/phq9/db/table.js'
+import tableRec from '~/components/pt-info/single/1t-Mrow-1Field/rec/db/vuex-orm/model.js'
+import tableDx from '~/components/pt-info/single/1t-Mrow-mField/dx/db/vuex-orm/Dx.js'
+import tableGoal from '~/components/pt-info/single/1t-Mrow-mField/goal/db/vuex-orm/Goal'
+import tableScr from '~/components/pt-info/single/1t-Mrow-mField/scr/db/vuex-orm/Screening'
+import tableDxa from '~/components/pt-info/single/1t-Mrow-mField/dx/db/vuex-orm/Dxa.js'
+import tablePhoneNumbers from '~/components/pt-info/single/1t-Mrow-mField/phone-numbers/db/vuex-orm/table.js'
+import tableCommon from '~/components/pt-info/single/1t-1row-mField/common-for-all-components/db/table.js'
 
 const database = new Database()
 
-database.register(CtSearch)
-database.register(Rem)
-database.register(ormName)
-database.register(ormWeight)
-database.register(ormHeight)
-database.register(ormBloodPressure)
-database.register(ormOxygenSaturation)
-database.register(ormBloodSugar)
-database.register(ormWaistCircumference)
-database.register(ormTemperature)
-database.register(ormPulse)
-database.register(ormDob)
-database.register(ormPhq9)
-database.register(Rec)
-database.register(Dx)
-database.register(Goal)
-database.register(Scr)
-database.register(CtLifeCycle)
-database.register(Dxa)
-database.register(phoneNumbers)
-database.register(objOrm)
+database.register(tableSearch)
+database.register(tableRem)
+database.register(tableName)
+database.register(tableWeight)
+database.register(tableHeight)
+database.register(tableBloodPressure)
+database.register(tableOxygenSaturation)
+database.register(tableBloodSugar)
+database.register(tableWaistCircumference)
+database.register(tableTemperature)
+database.register(tablePulse)
+database.register(tableDob)
+database.register(tablePhq9)
+database.register(tableRec)
+database.register(tableDx)
+database.register(tableGoal)
+database.register(tableScr)
+database.register(tableLifeCycle)
+database.register(tableDxa)
+database.register(tablePhoneNumbers)
+database.register(tableCommon)
 
 export default database
