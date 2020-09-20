dd010c703c124df84cfcfa311cc7ea9a830f253e
Vikas <kedia.vikas@gmail.com>
2020-09-19 18:58:53 -0700

    SS nextver see all at once

--- webclient/components/pt-info/single/1time-Mrow-1Field/service-statement/change-layer/add-ct.vue
+++ webclient/components/pt-info/single/1time-Mrow-1Field/service-statement/change-layer/add-ct.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <input v-model="userTypedKeyword" />
     <div class="grid-container">
       <div v-for="ss in cfArOfSSForDisplayInTable" :key="ss.id">
         <el-button>{{ ss.description }}</el-button>
@@ -11,10 +12,20 @@
 <script>
 import objMasterSSTable from '../db/client-side/structure/table-master-list-of-ss.js'
 export default {
+  data() {
+    return {
+      userTypedKeyword: '',
+    }
+  },
   computed: {
     cfArOfSSForDisplayInTable() {
-      const arFromOrm = objMasterSSTable.fnGetValidUniqueUuidNotEmptyRows('description')
-      return arFromOrm
+      const arOfObjectsFromClientSideDB = objMasterSSTable.fnGetValidUniqueUuidNotEmptyRows(
+        'description'
+      )
+      const newObj = arOfObjectsFromClientSideDB.filter((x) =>
+        x.description.includes(this.userTypedKeyword)
+      )
+      return newObj
     },
   },
   async mounted() {},
