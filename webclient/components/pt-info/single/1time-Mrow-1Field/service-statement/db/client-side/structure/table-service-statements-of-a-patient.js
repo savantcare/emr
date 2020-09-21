5fd1fe90ebffaca9b7c4f3deaa6ff9f89ff2fe19
Vikas <kedia.vikas@gmail.com>
2020-09-20 17:10:56 -0700

    bc

--- webclient/components/pt-info/single/1time-Mrow-1Field/service-statement/db/client-side/structure/table-service-statements-of-a-patient.js
+++ webclient/components/pt-info/single/1time-Mrow-1Field/service-statement/db/client-side/structure/table-service-statements-of-a-patient.js
@@ -19,15 +19,13 @@ export default class serviceStatements extends clientSideTableManage {
 
       clientSideUniqRowId: this.uid(() => intUniqueID()), // if this is not set then update based on primary key will not work
 
-      /* There is no extra field called serviceStatementUUID. This field is used to store the value of tblServiceStatementMaster/serverSideRowUuid
-         E.g: The  tblServiceStatementMaster has:
-         ServerSideRowUuid         |         serviceStatementDescription    
-          uuid1                    |  Spent 10 min with patient
-          uuid2                    |  Spent 20 min with patient
+      /* This field is used to store the value of tblServiceStatementsMaster/serviceStatementMasterId
+         E.g: The  tblServiceStatementsMaster has:
+         serviceStatementMasterId  |         serviceStatementDescription    
+              1                    |  Spent 10 min with patient
+              2                    |  Spent 20 min with patient
 
-          When doctor assigns uuid2 to this patient then in this table
-          serverSideRowUuid = 2
-         */
+          When doctor assigns 2 to this patient then in this table serviceStatementMasterId = 2 */
       serviceStatementMasterId: this.uid(() => uuidv1()), // This is service statement ID assigned to this patient coming from master table
 
       patientUUID: this.string(null),
@@ -38,7 +36,7 @@ export default class serviceStatements extends clientSideTableManage {
       ROW_START: this.number(0),
       ROW_END: this.number(2147483647.999999), // this is unix_timestamp value from mariaDB for ROW_END when a record is created new in MariaDB system versioned table.
 
-      // user: this.belongsTo(tblServiceStatementsMaster, 'serverSideRowUuid', ''),
+      //      user: this.belongsTo(serviceStatementsMasterList, 'serviceStatementMasterId', 'serviceStatementMasterId'),
     }
   }
 }
