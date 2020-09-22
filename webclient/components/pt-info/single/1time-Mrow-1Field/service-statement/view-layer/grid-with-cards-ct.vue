0e6ba4e041d167aa8697bc220d13066573c97add
Vikas <kedia.vikas@gmail.com>
2020-09-22 12:41:52 -0700

    mr

--- webclient/components/pt-info/single/1time-Mrow-1Field/service-statement/view-layer/grid-with-cards-ct.vue
+++ webclient/components/pt-info/single/1time-Mrow-1Field/service-statement/view-layer/grid-with-cards-ct.vue
@@ -1,54 +1,74 @@
 <template>
-  <div>
-    <el-card class="box-card" shadow="hover">
-      <div slot="header" class="clearfix">
-        <span>Service statements</span>
+  <el-card
+    shadow="hover"
+    class="box-card sc-service-statement-all-content"
+    :style="cfGetVariablesFromClientSideTableToUseInCSS"
+  >
+    <div
+      class="sc-service-statement-all-content-header clearfix"
+      slot="header"
+      v-on:click="mfOpenCCtInCl"
+    >
+      <span>Service statements</span>
+      <el-button-group style="float: right; display: none">
         <el-button
-          style="float: right; padding: 3px 0"
-          type="text"
+          style="padding: 3px; color: #c0c4cc; border: none"
+          plain
+          tabindex="-1"
           @click="mfOpenCCtInCl"
-          icon="el-icon-edit"
+          class="el-icon-circle-plus-outline"
         ></el-button>
-      </div>
-      <div class="grid-container data-card">
-        <el-card
-          v-for="ss in cfArOfServiceStatementForDisplay"
-          :key="ss.clientSideUniqRowId"
-          shadow="hover"
-          class="box-card content-card"
-        >
-          <div v-on:click="mfDeleteServiceStatement(ss.clientSideUniqRowId)">
-            <div class="info-icon">
-              <el-button type="text">
-                <el-tooltip
-                  class="item"
-                  effect="light"
-                  content="info"
-                  placement="top-end"
-                  :open-delay="500"
-                >
-                  <i class="el-icon-info custom-info-icon"></i>
-                </el-tooltip>
-              </el-button>
-            </div>
+      </el-button-group>
+    </div>
+    <div class="sc-service-statement-all-content-body">
+      <el-card
+        v-for="ss in cfArOfServiceStatementForDisplay"
+        :key="ss.clientSideUniqRowId"
+        class="box-card sc-service-statement-individual-card"
+        shadow="hover"
+        :style="mfGetCssClassName(ss)"
+      >
+        <div v-on:click="mfDeleteServiceStatement(ss.clientSideUniqRowId)">
+          <el-button-group style="float: right; display: none">
+            <el-button
+              class="el-icon-discover"
+              style="padding: 3px; color: #c0c4cc; border: none"
+              plain
+              tabindex="-1"
+              v-on:click.stop="mfShowTimeLine()"
+            >
+              <el-tooltip
+                class="item"
+                effect="light"
+                content="info"
+                placement="top-end"
+                :open-delay="500"
+              >
+              </el-tooltip>
+            </el-button>
 
-            <div class="delete-icon">
-              <el-button type="text" @click="mfDeleteServiceStatement(ss.clientSideUniqRowId)">
-                <i class="el-icon-error custom-close-icon"></i>
-              </el-button>
-            </div>
-            {{ ss.tblServiceStatementsMasterLink.serviceStatementCategory }}:
-            {{ ss.tblServiceStatementsMasterLink.serviceStatementDescription }}
-          </div>
-        </el-card>
-      </div>
-    </el-card>
-  </div>
+            <el-button
+              class="el-icon-circle-close"
+              style="padding: 3px; color: #c0c4cc; border: none"
+              plain
+              tabindex="-1"
+              @click="mfDeleteServiceStatement(ss.clientSideUniqRowId)"
+            >
+            </el-button>
+          </el-button-group>
+
+          {{ ss.tblServiceStatementsMasterLink.serviceStatementCategory }}:
+          {{ ss.tblServiceStatementsMasterLink.serviceStatementDescription }}
+        </div>
+      </el-card>
+    </div>
+  </el-card>
 </template>
 
 <script>
 import clientSideTblMasterServiceStatements from '../db/client-side/structure/table-master-list-of-service-statements.js'
 import clientSideTblPatientServiceStatements from '../db/client-side/structure/table-service-statements-of-a-patient.js'
+import objCommonOrm from '@/components/pt-info/single/1time-1row-mField/common-for-all-components/db/client-side/structure/table.js'
 
 export default {
   computed: {
@@ -59,6 +79,18 @@ export default {
         .where('ROW_END', 2147483647.999999)
         .get()
       return arOfObjectsFromClientSideDB
+    },
+    cfGetVariablesFromClientSideTableToUseInCSS() {
+      const objCommonRow = objCommonOrm.find(1) // if this is async then it does not work.
+      if (objCommonRow !== null) {
+        let fontSizeOfContentEveryWhere = objCommonRow.fontSizeOfContentEveryWhereForNormalEyeSight
+        if (objCommonRow.currentUserEyeSight == 'weak') {
+          fontSizeOfContentEveryWhere = objCommonRow.fontSizeOfContentEveryWhereForWeakEyeSight
+        }
+        return {
+          '--font-size-of-content-every-where': fontSizeOfContentEveryWhere,
+        }
+      }
     },
   },
   methods: {
@@ -75,17 +107,89 @@ export default {
         searchTerm: 'change service statement',
       })
     },
+    // This is used to make the rows that are in change state a orange background.
+    mfGetCssClassName(pRow) {
+      const strOfNumber = pRow.vnRowStateInSession.toString()
+      const lastCharecter = strOfNumber.slice(-1)
+      if (lastCharecter === '4' || lastCharecter === '6') {
+        return 'color: #E6A23C;'
+      } else {
+        return 'color: #202020;'
+      }
+    },
+    mfShowTimeLine: () => {
+      alert('Timeline of this data')
+      return false
+    },
   },
   async mounted() {},
 }
 </script>
 
 <style>
-.grid-container {
+/* Generation ->
+==============
+                                       .sc-service-statement-all-content
+ Generation 1                                      |
+==============                     _________________________________
+                                  |                                |
+                    .sc-service-statement-all-content-header                |
+ Generatiobn 2                                                     |
+                                    _______________________________|
+                                  |
+                  .sc-service-statement-all-content-body
+==============                    |
+                                  |
+                   .sc-service-statement-individual-card
+Generatiobn 3                     |
+                       ________________________________________________________________________________
+                      |                                 |                                             |
+        .sc-service-statement-individual-card-content    .sc-service-statement-individual-card-info-icon           .sc-service-statement-individual-card-delete-icon
+
+==============
+*/
+
+/* Generation Level 1 */
+.sc-service-statement-all-content .el-card__header {
+  /* Goal: Manage Distance from border to content in header*/
+  padding: 10px !important;
+}
+
+.sc-service-statement-all-content .el-card__body {
+  /* Goal: Manage  Distance from border to content in body*/
+  padding: 10px !important;
+}
+
+/* Generation Level 2 / Child 1 == Goal: Header icon management */
+
+/* When anywhere inside the card make the action icons in the card header -> level 1 visual */
+.el-card:hover .sc-service-statement-all-content-header .el-button-group {
+  display: inline-block !important;
+}
+
+/* When inside the card header make the action icons in the card header -> level 2 visual */
+.el-card__header:hover .sc-service-statement-all-content-header .el-icon-circle-plus-outline {
+  color: #67c23a !important;
+  font-size: 20px;
+}
+.el-card__header:hover .sc-service-statement-all-content-header .el-icon-s-grid {
+  color: #409eff !important;
+}
+.el-card__header:hover .sc-service-statement-all-content-header .el-icon-document-delete {
+  color: #f56c6c !important;
+}
+.el-card__header:hover .sc-service-statement-all-content-header .el-icon-delete {
+  color: #909399 !important;
+}
+
+/* Generation Level 2 / Child 2 */
+
+/* Goal: When less space display 1 card in a row. When more space display 100 cards in a row. */
+.sc-service-statement-all-content-body {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   /* Some other grid-template-columns options are :
-  grid-template-columns: repeat(auto-fit, minmax(32rem, 1fr)); 
+  grid-template-columns: repeat(auto-fit, minmax(32rem, 1fr));
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   grid-template-columns: repeat(auto-fit, max(200px)); compared to minmax(200px, 1fr) there is more magin between cols and less content fits.
   */
@@ -93,81 +197,35 @@ export default {
   grid-auto-flow: row; /* This is default value */
   margin: 1px;
 }
-.el-tag {
-  margin-left: 10px;
-  cursor: pointer;
-}
-.el-tag .el-tag__close {
-  color: #409eff;
-  float: right;
-  top: 7px !important;
-}
-.grid-container.data-card .el-card__body {
-  padding: 16px 10px 5px;
-  height: 100%;
-  line-height: 18px;
-  text-decoration: none;
-}
-.delete-icon {
-  position: absolute;
-  top: -10px;
-  right: 0px;
-  z-index: 999;
-  cursor: pointer;
-  text-decoration: none;
-}
-.delete-icon .el-button.el-button--text {
-  color: #ebe9e9;
-}
-.content-card.el-card.box-card:hover .delete-icon .el-button.el-button--text {
-  color: #f39797;
-}
-.content-card {
+
+/* Generation Level 3 */
+
+/* Goal: Margin and padding of individual card */
+
+.sc-service-statement-individual-card {
   cursor: pointer;
   overflow-wrap: break-word;
   position: relative;
-  margin: 5px;
-}
-.content-div {
-  font-size: 12px;
-  height: 100%;
-}
-.content-div:hover {
-  color: #409eff !important;
-}
-.content-card.el-card.box-card {
-  border: 1px solid #e4e4e4;
-}
-.content-card.el-card.box-card:hover {
-  border: 1px solid #b7daf7;
-}
-.custom-close-icon {
-  font-size: 0.85rem;
-}
-.custom-close-icon:hover {
-  color: #ff0000;
-  font-size: 1.05rem;
+  margin: 1px;
+  padding: 10px 1px 1px !important;
+  /* Goal: Give normal and weak eyesight two different font size*/
+  font-size: var(--font-size-of-content-every-where);
 }
 
-.info-icon {
+/* Goal: Header icon management  */
+.el-card:hover .sc-service-statement-individual-card .el-button-group {
+  display: inline-block !important;
   position: absolute;
-  top: -10px;
-  right: 16px;
-  z-index: 999;
-  cursor: pointer;
-  text-decoration: none;
+  top: 0px;
+  right: 0px;
 }
-.info-icon .el-button.el-button--text {
-  color: #ebe9e9;
+
+.sc-service-statement-individual-card:hover .el-icon-discover {
+  color: #909399 !important;
 }
-.content-card.el-card.box-card:hover .info-icon .el-button.el-button--text {
-  color: #a1e5fa;
-}
-.custom-info-icon {
-  font-size: 0.85rem;
-}
-.custom-info-icon:hover {
-  color: #2ccbfc;
-  font-size: 1.05rem;
+
+.sc-service-statement-individual-card:hover .el-icon-circle-close {
+  color: #f56c6c !important;
+  font-size: 20px;
 }
 </style>
