9968e4259598bfc35de1fd61cc9bead3bcca5898
Vikas <kedia.vikas@gmail.com>
2020-09-30 00:11:47 -0700

    DOB works

--- webclient/components/pt-info/single/1time-1row-mField/date-of-birth/view-layer/dob-table-ct.vue
+++ webclient/components/pt-info/single/1time-1row-mField/date-of-birth/view-layer/dob-table-ct.vue
@@ -3,54 +3,81 @@ Code synced with ref implementation on 4th august 2020
  -->
 <template>
   <div>
-    <h5 v-if="formType === 'stand-alone'">Date of birth</h5>
-    <el-button
-      :type="mfTypeOfButton('dateOfBirthInMilliseconds')"
-      plain
-      :tabindex="cfPosInArCardsInPtsOfVl * 100 + 1"
-      size="small"
-      >{{ cfFormatDateOfBirth }}</el-button
+    <showContentInCardComponent
+      propMainCardName="Date of birth"
+      :propClientSideTableLevelActions="[
+        {
+          actionDescription: 'Toggle card display',
+          actionUIByElementIoIconClass: 'el-icon-remove-outline',
+        },
+        { actionDescription: 'Close card', actionUIByElementIoIconClass: 'el-icon-close' },
+      ]"
+      propGridDesignTopMostParentBody="s-css-class-top-most-card-body-grid"
     >
-    <el-button :type="mfTypeOfButton('notes')" plain size="small">{{
-      cfLatestDataRowFromClientSideTable['notes']
-    }}</el-button>
-    <el-button
-      type="primary"
-      size="mini"
-      style="padding: 3px"
-      plain
-      tabindex="-1"
-      @click="mfOpenEditCtInEditLayer(cfLatestDataRowFromClientSideTable['clientSideUniqRowId'])"
-      class="el-icon-edit"
-    ></el-button>
-    <el-button
-      v-if="dataFldsOfToChangeAndCopiedRowsAreSame !== true"
-      type="success"
-      size="mini"
-      style="padding: 3px"
-      plain
-      tabindex="-1"
-      @click="mfSendReviewedEvent()"
-      >S</el-button
-    >
-    <el-button
-      v-if="dataFldsOfToChangeAndCopiedRowsAreSame !== true"
-      type="danger"
-      size="mini"
-      style="padding: 3px"
-      plain
-      tabindex="-1"
-      @click="mfSendResetFormEvent()"
-      >R</el-button
-    >
+      <div
+        slot="bodySlotContentFromParentToShowAboveChildCards"
+        class="box-card sc-individual-child-card"
+      >
+        <el-button-group style="float: right; display: none">
+          <el-button
+            type="primary"
+            size="mini"
+            style="padding: 3px"
+            plain
+            tabindex="-1"
+            @click="
+              mfOpenEditCtInEditLayer(
+                cfLatestDataRowFromClientSideTable['clientSideUniqRowId'],
+                'date of birth - edit'
+              )
+            "
+            class="el-icon-edit"
+          ></el-button>
+          <el-button
+            v-if="dataFldsOfToChangeAndCopiedRowsAreSame !== true"
+            type="success"
+            size="mini"
+            style="padding: 3px"
+            plain
+            tabindex="-1"
+            @click="mfSendReviewedEvent()"
+            >S</el-button
+          >
+          <el-button
+            v-if="dataFldsOfToChangeAndCopiedRowsAreSame !== true"
+            type="danger"
+            size="mini"
+            style="padding: 3px"
+            plain
+            tabindex="-1"
+            @click="mfSendResetFormEvent()"
+            >R</el-button
+          >
+        </el-button-group>
+
+        <el-button
+          :type="mfTypeOfButton('dateOfBirthInMilliseconds')"
+          plain
+          :tabindex="cfPosInArCardsInPtsOfVl * 100 + 1"
+          size="small"
+          >{{ cfFormatDateOfBirth }}</el-button
+        >
+        <el-button :type="mfTypeOfButton('notes')" plain size="small">{{
+          cfLatestDataRowFromClientSideTable['notes']
+        }}</el-button>
+      </div>
+    </showContentInCardComponent>
   </div>
 </template>
 
 <script>
 import moment from 'moment'
-
 import mxViewLayer from '../code-common-for-all-1r-mf/view-layer.js'
+import showContentInCardComponent from '@/components/pt-info/single/common/show-content-in-card-component.vue'
+
 export default {
+  components: { showContentInCardComponent },
+
   mixins: [mxViewLayer],
   props: {
     formType: {
