46c7612abbc8af7702a564df561ac184f179c659
Raj Roushan Jha <raj@grmtech.com>
2020-10-01 18:54:00 +0530

    Now blood sugar api running from lumen

--- webclient/components/pt-info/single/1time-1row-mField/bm/sub-cts/blood-sugar/db/server-side/structure.sql
+++ webclient/components/pt-info/single/1time-1row-mField/bm/sub-cts/blood-sugar/db/server-side/structure.sql
@@ -1,14 +1,21 @@
-use sc_rem;
+use sc_body_measurement;
 
-DROP TABLE IF EXISTS `rems`;
+DROP TABLE IF EXISTS `bloodSugar`;
 
-CREATE TABLE `rems` (
-  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
-  `firstName` char(36) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `middleName` char(36) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `lastName` char(36) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `recordChangedByUUID` char(36) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `recordChangedFromIPAddress` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `recordChangedFromSection` varchar(255) COLLATE utf8_unicode_ci DEFAULT 'patientFile',
-  PRIMARY KEY (`uuid`)
+CREATE TABLE `bloodSugar` (
+  `serverSideRowUuid` char(36) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `ptUUID` char(36) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bloodSugarInBpm` int(11) DEFAULT NULL,
+  `timeOfMeasurementInMilliseconds` datetime(3) NOT NULL DEFAULT current_timestamp(3),
+  `notes` text DEFAULT NULL,
+  `recordChangedByUUID` char(36) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `recordChangedFromIPAddress` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`serverSideRowUuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 WITH SYSTEM VERSIONING;
+
+--
+-- Dumping data for table `bloodSugar`
+--
+
+INSERT INTO `bloodSugar` (`serverSideRowUuid`, `ptUUID`, `bloodSugarInBpm`, `notes`, `recordChangedByUUID`, `recordChangedFromIPAddress`) VALUES
+('01817fb0-c1ef-11ea-a3a5-f36fe4d74da4', 'bfe041fa-073b-4223-8c69-0540ee678ff8', 75, 'test', 'bfe041fa-073b-4223-8c69-0540ee678ff8', NULL); 
