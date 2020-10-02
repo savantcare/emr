fd966945cb273c6ad5c017eb6023c9916f822532
Vikas <kedia.vikas@gmail.com>
2020-10-02 04:23:26 -0700

    +display

--- webclient/components/pt-info/single/1time-1row-mField/psych-review-of-systems/view-layer/pros-design-1-ct.vue
+++ webclient/components/pt-info/single/1time-1row-mField/psych-review-of-systems/view-layer/pros-design-1-ct.vue
@@ -15,7 +15,60 @@
       },
     ]"
     :propClientSideRowLevelActions="[{}]"
-  />
+  >
+    <el-card
+      slot="bodySlotContentFromParentToShowAboveChildCards"
+      v-for="pros in cfArOfPsychReviewOfSystemsForDisplay"
+      :key="pros.id"
+      class="box-card sc-individual-child-card"
+      shadow="hover"
+      :style="mfGetCssClassNameForEachDataRow(pros)"
+    >
+      <el-button-group style="float: right; display: none">
+        <el-tooltip
+          class="item"
+          effect="light"
+          content="Click to delete"
+          placement="top-end"
+          :open-delay="500"
+        >
+          <el-button
+            style="padding: 3px; color: #c0c4cc; border: none"
+            plain
+            @click="mfIconDeleteClickedOnChildCard(pros.clientSideUniqRowId)"
+            class="el-icon-circle-close"
+          >
+          </el-button>
+        </el-tooltip>
+        <el-tooltip
+          class="item"
+          effect="light"
+          content="info"
+          placement="top-end"
+          :open-delay="500"
+        >
+          <el-button
+            style="padding: 3px; color: #c0c4cc; border: none"
+            plain
+            class="el-icon-discover"
+          >
+          </el-button>
+        </el-tooltip>
+      </el-button-group>
+
+      <!-- <el-button type="text">{{ pros.description }}</el-button> 
+          if I use the button then a long text is not getting divided into multiple lines
+          if rowStateInThisSession == 9 then the div should have a orange border
+          Why we are doing this?
+            Doctor is sitting infront of computer suddenly a new pros appears. That is a confusing event.
+            Instead if the new pros that came on screen gets a orange border with top right corner saying "New pros added from socket" that is much better UX.
+          -->
+      <div v-if="pros.vnRowStateInSession === 9">Added from socket {{ pros.description }}</div>
+      <div v-else>
+        {{ pros.cardContentOfTypeStringToShowInBodyOfCards }}
+      </div>
+    </el-card>
+  </showContentInCardComponent>
 </template>
 
 <script>
@@ -33,6 +86,20 @@ export default {
         .with('tblPsychReviewOfSystemsMasterLink')
         .where('ROW_END', 2147483647.999999)
         .get()
+
+      console.log(arOfObjectsFromClientSideDB)
+
+      for (var i = 0; i < arOfObjectsFromClientSideDB.length; i++) {
+        arOfObjectsFromClientSideDB[i]['cardContentOfTypeStringToShowInBodyOfCards'] =
+          arOfObjectsFromClientSideDB[i].tblPsychReviewOfSystemsMasterLink
+            .psychReviewOfSystemsCategory +
+          ': ' +
+          arOfObjectsFromClientSideDB[i].tblPsychReviewOfSystemsMasterLink
+            .psychReviewOfSystemsDescription
+      }
+
+      console.log(arOfObjectsFromClientSideDB)
+
       return arOfObjectsFromClientSideDB
     },
   },
@@ -49,6 +116,16 @@ export default {
       this.$store.commit('mtfShowNewFirstTabInEditLayerFromSearchPhrase', {
         searchTerm: 'edit psych review of systems',
       })
+    },
+    // This is used to make the rows that are in change state a orange background.
+    mfGetCssClassNameForEachDataRow(pRow) {
+      const strOfNumber = pRow.vnRowStateInSession.toString()
+      const lastCharecter = strOfNumber.slice(-1)
+      if (lastCharecter === '4' || lastCharecter === '6') {
+        return 'color: #E6A23C;'
+      } else {
+        return 'color: #202020;'
+      }
     },
   },
   async mounted() {},
