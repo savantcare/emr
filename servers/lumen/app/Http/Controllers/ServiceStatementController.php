8b3d4dee6281f6f1e7965aa13c0f8b2f63060644
Vikas <kedia.vikas@gmail.com>
2020-10-04 16:57:13 -0700

    SS get query works

--- servers/lumen/app/Http/Controllers/ServiceStatementController.php
+++ servers/lumen/app/Http/Controllers/ServiceStatementController.php
@@ -15,7 +15,7 @@ class ServiceStatementController extends Controller
 {
     public function showAllServiceStatements()
     {
-        $ServiceStatementQuery = DB::select(DB::raw('SELECT *, round(UNIX_TIMESTAMP(ROW_START) * 1000) as ROW_START, round(UNIX_TIMESTAMP(ROW_END) * 1000) as ROW_END FROM service_statements FOR SYSTEM_TIME ALL order by ROW_START desc'));
+        $ServiceStatementQuery = DB::select(DB::raw('SELECT *, round(UNIX_TIMESTAMP(ROW_START) * 1000) as ROW_START, round(UNIX_TIMESTAMP(ROW_END) * 1000) as ROW_END FROM sc_service_statements.service_statements FOR SYSTEM_TIME ALL order by ROW_START desc'));
         return response()->json($ServiceStatementQuery);
         // return response()->json(ServiceStatement::all());
     }
