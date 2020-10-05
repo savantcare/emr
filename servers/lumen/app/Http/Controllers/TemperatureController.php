7854c9b0f1d8d410bdf7d454bd23acb112f1d9f2
Vikas <kedia.vikas@gmail.com>
2020-10-04 23:03:04 -0700

    +read

--- servers/lumen/app/Http/Controllers/TemperatureController.php
+++ servers/lumen/app/Http/Controllers/TemperatureController.php
@@ -5,6 +5,7 @@ use App\Temperature;
 use Illuminate\Http\Request;
 use DB;
 use Predis\Autoloader;
+
 \Predis\Autoloader::register();
 
 class TemperatureController extends Controller
@@ -22,6 +23,16 @@ class TemperatureController extends Controller
 
         return response()->json($temperatureQuery);
     }
+
+
+    /*
+    To check temperature/create in postman make a post request with the following JSON:
+    Open postman desktop app and then:
+    URL: http://localhost:8000/public/api/temperature/v20/
+    Post / Body raw: {"rowToUpsert":{"$id":"3","vnRowStateInSession":34,"validationClass":"","isValidationError":false,"clientSideUniqRowId":3,"serverSideRowUuid":"01817fb0-c1ef-11ea-a3a5-f36fe4d74da4","temperatureInFarehnite":100,"timeOfMeasurementInMilliseconds":1601876131994,"notes":"test","recordChangedByUUID":"bfe041fa-073b-4223-8c69-0540ee678ff8","recordChangedFromIPAddress":"::1","recordChangedFromSection":"null","ROW_START":1601877241687,"ROW_END":2147483648000}}
+    */
+
+
 
     public function create(Request $request)
     {
@@ -55,23 +66,24 @@ class TemperatureController extends Controller
         return response()->json($updateTempereture, 200);
     }
 
-    public function get_client_ip() {
+    public function get_client_ip()
+    {
         $ipaddress = '';
-        if (isset($_SERVER['HTTP_CLIENT_IP']))
+        if (isset($_SERVER['HTTP_CLIENT_IP'])) {
             $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        } elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        } elseif (isset($_SERVER['HTTP_X_FORWARDED'])) {
             $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        } elseif (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
             $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-        else if(isset($_SERVER['HTTP_FORWARDED']))
+        } elseif (isset($_SERVER['HTTP_FORWARDED'])) {
             $ipaddress = $_SERVER['HTTP_FORWARDED'];
-        else if(isset($_SERVER['REMOTE_ADDR']))
+        } elseif (isset($_SERVER['REMOTE_ADDR'])) {
             $ipaddress = $_SERVER['REMOTE_ADDR'];
-        else
+        } else {
             $ipaddress = 'UNKNOWN';
+        }
         return $ipaddress;
     }
-
-}  
+}
