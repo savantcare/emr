df3997e8bcf9d48d005535f1b34d389fc6db3be2
Vikas <kedia.vikas@gmail.com>
2020-10-07 00:42:10 -0700

    structure

---
+++ webclient/components/ptinfo-single/1time-1row-mField/common-for-all-components/db/client-side/static-data/insert-into-config-table.vue
@@ -0,0 +1,43 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+/*
+import clientSideTblOfCtConfig from '@/components/'
+
+export default {
+  data() {
+    return {}
+  },
+  mounted() {
+    clientSideTblOfCtSearchPhrases.insert({
+      data: {
+        value: 'reminders',
+        ctAbbr: 'rem',
+        ctToShow: 'ptinfo-single/1time-Mrow-1Field/reminder/view-layer/timeline-ct.vue', // PtsVl -> Present time state - part of - view layer
+        displayLocation: 'PresentTimeStateViewLayer',
+      },
+    })
+    clientSideTblOfCtSearchPhrases.insert({
+      data: {
+        value: 'add reminder',
+        ctAbbr: 'arem',
+        ctToShow: 'ptinfo-single/1time-Mrow-1Field/reminder/edit-layer/add-ct.vue',
+        displayLocation: 'edit-layer',
+        ctWidthInEditLayer: 'small',
+      },
+    })
+    clientSideTblOfCtSearchPhrases.insert({
+      data: {
+        value: 'edit reminder',
+        ctAbbr: 'crem',
+        ctToShow: 'ptinfo-single/1time-Mrow-1Field/reminder/edit-layer/edit-design-1.vue',
+        displayLocation: 'edit-layer',
+        ctWidthInEditLayer: 'small',
+        needsRowIdToWork: 'yes',
+      },
+    })
+  },
+}*/
+</script>
