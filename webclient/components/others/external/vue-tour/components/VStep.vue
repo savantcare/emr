b1f850e04d9be92543ddd576b5bbc39c0a79c3e2
Vikas <kedia.vikas@gmail.com>
2020-10-08 02:12:30 -0700

    steps now use el-button class

--- webclient/components/others/external/vue-tour/components/VStep.vue
+++ webclient/components/others/external/vue-tour/components/VStep.vue
@@ -9,20 +9,45 @@
     <slot name="content">
       <div class="v-step__content">
         <div v-if="step.content" v-html="step.content"></div>
-        <div v-else>This is a demo step! The id of this step is {{ hash }} and it targets {{ step.target }}.</div>
+        <div v-else>
+          This is a demo step! The id of this step is {{ hash }} and it targets {{ step.target }}.
+        </div>
       </div>
     </slot>
 
     <slot name="actions">
       <div class="v-step__buttons">
-        <button @click.prevent="skip" v-if="!isLast && isButtonEnabled('buttonSkip')" class="v-step__button v-step__button-skip">{{ labels.buttonSkip }}</button>
-        <button @click.prevent="previousStep" v-if="!isFirst && isButtonEnabled('buttonPrevious')" class="v-step__button v-step__button-previous">{{ labels.buttonPrevious }}</button>
-        <button @click.prevent="nextStep" v-if="!isLast && isButtonEnabled('buttonNext')" class="v-step__button v-step__button-next">{{ labels.buttonNext }}</button>
-        <button @click.prevent="finish" v-if="isLast && isButtonEnabled('buttonStop')" class="v-step__button v-step__button-stop">{{ labels.buttonStop }}</button>
+        <el-button
+          @click.prevent="skip"
+          v-if="!isLast && isButtonEnabled('buttonSkip')"
+          class="v-step__button v-step__button-skip"
+          >{{ labels.buttonSkip }}</el-button
+        >
+        <el-button
+          @click.prevent="previousStep"
+          v-if="!isFirst && isButtonEnabled('buttonPrevious')"
+          class="v-step__button v-step__button-previous"
+          >{{ labels.buttonPrevious }}</el-button
+        >
+        <el-button
+          @click.prevent="nextStep"
+          v-if="!isLast && isButtonEnabled('buttonNext')"
+          class="v-step__button v-step__button-next"
+          >{{ labels.buttonNext }}</el-button
+        >
+        <el-button
+          @click.prevent="finish"
+          v-if="isLast && isButtonEnabled('buttonStop')"
+          class="v-step__button v-step__button-stop"
+          >{{ labels.buttonStop }}</el-button
+        >
       </div>
     </slot>
 
-    <div class="v-step__arrow" :class="{ 'v-step__arrow--dark': step.header && step.header.title }"></div>
+    <div
+      class="v-step__arrow"
+      :class="{ 'v-step__arrow--dark': step.header && step.header.title }"
+    ></div>
   </div>
 </template>
 
@@ -36,71 +61,78 @@ export default {
   name: 'v-step',
   props: {
     step: {
-      type: Object
+      type: Object,
     },
     previousStep: {
-      type: Function
+      type: Function,
     },
     nextStep: {
-      type: Function
+      type: Function,
     },
     stop: {
-      type: Function
+      type: Function,
     },
     skip: {
       type: Function,
       default: function () {
         this.stop()
-      }
+      },
     },
     finish: {
       type: Function,
       default: function () {
         this.stop()
-      }
+      },
     },
     isFirst: {
-      type: Boolean
+      type: Boolean,
     },
     isLast: {
-      type: Boolean
+      type: Boolean,
     },
     labels: {
-      type: Object
+      type: Object,
     },
     enabledButtons: {
-      type: Object
+      type: Object,
     },
     highlight: {
-      type: Boolean
+      type: Boolean,
     },
     stopOnFail: {
-      type: Boolean
+      type: Boolean,
     },
     debug: {
-      type: Boolean
-    }
+      type: Boolean,
+    },
   },
-  data () {
+  data() {
     return {
       hash: sum(this.step.target),
-      targetElement: document.querySelector(this.step.target)
+      targetElement: document.querySelector(this.step.target),
     }
   },
   computed: {
-    params () {
+    params() {
       return {
         ...DEFAULT_STEP_OPTIONS,
         ...{ highlight: this.highlight }, // Use global tour highlight setting first
         ...{ enabledButtons: Object.assign({}, this.enabledButtons) },
-        ...this.step.params // Then use local step parameters if defined
+        ...this.step.params, // Then use local step parameters if defined
       }
-    }
+    },
   },
   methods: {
-    createStep () {
+    createStep() {
       if (this.debug) {
-        console.log('[Vue Tour] The target element ' + this.step.target + ' of .v-step[id="' + this.hash + '"] is:', this.targetElement)
+        console.log(
+          '[Vue Tour] The target element ' +
+            this.step.target +
+            ' of .v-step[id="' +
+            this.hash +
+            '"] is:',
+          this.targetElement
+        )
       }
 
       if (this.targetElement) {
@@ -108,14 +140,16 @@ export default {
         this.createHighlight()
 
         /* eslint-disable no-new */
-        new Popper(
-          this.targetElement,
-          this.$refs['v-step-' + this.hash],
-          this.params
-        )
+        new Popper(this.targetElement, this.$refs['v-step-' + this.hash], this.params)
       } else {
         if (this.debug) {
-          console.error('[Vue Tour] The target element ' + this.step.target + ' of .v-step[id="' + this.hash + '"] does not exist!')
+          console.error(
+            '[Vue Tour] The target element ' +
+              this.step.target +
+              ' of .v-step[id="' +
+              this.hash +
+              '"] does not exist!'
+          )
         }
         this.$emit('targetNotFound', this.step)
         if (this.stopOnFail) {
@@ -123,14 +157,14 @@ export default {
         }
       }
     },
-    enableScrolling () {
+    enableScrolling() {
       if (this.params.enableScrolling) {
         if (this.step.duration || this.step.offset) {
           let jumpOptions = {
             duration: this.step.duration || 1000,
             offset: this.step.offset || 0,
             callback: undefined,
-            a11y: false
+            a11y: false,
           }
 
           jump(this.targetElement, jumpOptions)
@@ -140,16 +174,22 @@ export default {
         }
       }
     },
-    isHighlightEnabled () {
+    isHighlightEnabled() {
       if (this.debug) {
-        console.log(`[Vue Tour] Highlight is ${this.params.highlight ? 'enabled' : 'disabled'} for .v-step[id="${this.hash}"]`)
+        console.log(
+          `[Vue Tour] Highlight is ${
+            this.params.highlight ? 'enabled' : 'disabled'
+          } for .v-step[id="${this.hash}"]`
+        )
       }
       return this.params.highlight
     },
-    createHighlight () {
+    createHighlight() {
       if (this.isHighlightEnabled()) {
         document.body.classList.add(HIGHLIGHT.CLASSES.ACTIVE)
-        const transitionValue = window.getComputedStyle(this.targetElement).getPropertyValue('transition')
+        const transitionValue = window
+          .getComputedStyle(this.targetElement)
+          .getPropertyValue('transition')
 
         // Make sure our background doesn't flick on transitions
         if (transitionValue !== 'all 0s ease 0s') {
@@ -165,7 +205,7 @@ export default {
         document.body.classList.remove(HIGHLIGHT.CLASSES.ACTIVE)
       }
     },
-    removeHighlight () {
+    removeHighlight() {
       if (this.isHighlightEnabled()) {
         const target = this.targetElement
         const currentTransition = this.targetElement.style.transition
@@ -179,143 +219,145 @@ export default {
         }
       }
     },
-    isButtonEnabled (name) {
-      return this.params.enabledButtons.hasOwnProperty(name) ? this.params.enabledButtons[name] : true
-    }
+    isButtonEnabled(name) {
+      return this.params.enabledButtons.hasOwnProperty(name)
+        ? this.params.enabledButtons[name]
+        : true
+    },
   },
-  mounted () {
+  mounted() {
     this.createStep()
   },
-  destroyed () {
+  destroyed() {
     this.removeHighlight()
-  }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-  .v-step {
-    background: #50596c; /* #ffc107, #35495e */
-    color: white;
-    max-width: 320px;
-    border-radius: 3px;
-    filter: drop-shadow(0 0 2px rgba(0, 0, 0, 0.5));
-    padding: 1rem;
-    text-align: center;
-    z-index: 10000;
+.v-step {
+  background: #50596c; /* #ffc107, #35495e */
+  color: white;
+  max-width: 320px;
+  border-radius: 3px;
+  filter: drop-shadow(0 0 2px rgba(0, 0, 0, 0.5));
+  padding: 1rem;
+  text-align: center;
+  z-index: 10000;
+}
+
+.v-step .v-step__arrow {
+  width: 0;
+  height: 0;
+  border-style: solid;
+  position: absolute;
+  margin: 0.5rem;
+}
+
+.v-step .v-step__arrow {
+  border-color: #50596c; /* #ffc107, #35495e */
+
+  &--dark {
+    border-color: #454d5d;
   }
+}
 
-  .v-step .v-step__arrow {
-    width: 0;
-    height: 0;
-    border-style: solid;
-    position: absolute;
-    margin: 0.5rem;
+.v-step[x-placement^='top'] {
+  margin-bottom: 0.5rem;
+}
+
+.v-step[x-placement^='top'] .v-step__arrow {
+  border-width: 0.5rem 0.5rem 0 0.5rem;
+  border-left-color: transparent;
+  border-right-color: transparent;
+  border-bottom-color: transparent;
+  bottom: -0.5rem;
+  left: calc(50% - 1rem);
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.v-step[x-placement^='bottom'] {
+  margin-top: 0.5rem;
+}
+
+.v-step[x-placement^='bottom'] .v-step__arrow {
+  border-width: 0 0.5rem 0.5rem 0.5rem;
+  border-left-color: transparent;
+  border-right-color: transparent;
+  border-top-color: transparent;
+  top: -0.5rem;
+  left: calc(50% - 1rem);
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.v-step[x-placement^='right'] {
+  margin-left: 0.5rem;
+}
+
+.v-step[x-placement^='right'] .v-step__arrow {
+  border-width: 0.5rem 0.5rem 0.5rem 0;
+  border-left-color: transparent;
+  border-top-color: transparent;
+  border-bottom-color: transparent;
+  left: -0.5rem;
+  top: calc(50% - 1rem);
+  margin-left: 0;
+  margin-right: 0;
+}
+
+.v-step[x-placement^='left'] {
+  margin-right: 0.5rem;
+}
+
+.v-step[x-placement^='left'] .v-step__arrow {
+  border-width: 0.5rem 0 0.5rem 0.5rem;
+  border-top-color: transparent;
+  border-right-color: transparent;
+  border-bottom-color: transparent;
+  right: -0.5rem;
+  top: calc(50% - 1rem);
+  margin-left: 0;
+  margin-right: 0;
+}
+
+/* Custom */
+
+.v-step__header {
+  margin: -1rem -1rem 0.5rem;
+  padding: 0.5rem;
+  background-color: #454d5d;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+}
+
+.v-step__content {
+  margin: 0 0 1rem 0;
+}
+
+.v-step__button {
+  background: transparent;
+  border: 0.05rem solid white;
+  border-radius: 0.1rem;
+  color: white;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 0.8rem;
+  height: 1.8rem;
+  line-height: 1rem;
+  outline: none;
+  margin: 0 0.2rem;
+  padding: 0.35rem 0.4rem;
+  text-align: center;
+  text-decoration: none;
+  transition: all 0.2s ease;
+  vertical-align: middle;
+  white-space: nowrap;
+
+  &:hover {
+    background-color: rgba(white, 0.95);
+    color: #50596c;
   }
-
-  .v-step .v-step__arrow {
-    border-color: #50596c; /* #ffc107, #35495e */
-
-    &--dark {
-      border-color: #454d5d;
-    }
-  }
-
-  .v-step[x-placement^="top"] {
-    margin-bottom: 0.5rem;
-  }
-
-  .v-step[x-placement^="top"] .v-step__arrow {
-    border-width: 0.5rem 0.5rem 0 0.5rem;
-    border-left-color: transparent;
-    border-right-color: transparent;
-    border-bottom-color: transparent;
-    bottom: -0.5rem;
-    left: calc(50% - 1rem);
-    margin-top: 0;
-    margin-bottom: 0;
-  }
-
-  .v-step[x-placement^="bottom"] {
-    margin-top: 0.5rem;
-  }
-
-  .v-step[x-placement^="bottom"] .v-step__arrow {
-    border-width: 0 0.5rem 0.5rem 0.5rem;
-    border-left-color: transparent;
-    border-right-color: transparent;
-    border-top-color: transparent;
-    top: -0.5rem;
-    left: calc(50% - 1rem);
-    margin-top: 0;
-    margin-bottom: 0;
-  }
-
-  .v-step[x-placement^="right"] {
-    margin-left: 0.5rem;
-  }
-
-  .v-step[x-placement^="right"] .v-step__arrow {
-    border-width: 0.5rem 0.5rem 0.5rem 0;
-    border-left-color: transparent;
-    border-top-color: transparent;
-    border-bottom-color: transparent;
-    left: -0.5rem;
-    top: calc(50% - 1rem);
-    margin-left: 0;
-    margin-right: 0;
-  }
-
-  .v-step[x-placement^="left"] {
-    margin-right: 0.5rem;
-  }
-
-  .v-step[x-placement^="left"] .v-step__arrow {
-    border-width: 0.5rem 0 0.5rem 0.5rem;
-    border-top-color: transparent;
-    border-right-color: transparent;
-    border-bottom-color: transparent;
-    right: -0.5rem;
-    top: calc(50% - 1rem);
-    margin-left: 0;
-    margin-right: 0;
-  }
-
-  /* Custom */
-
-  .v-step__header {
-    margin: -1rem -1rem 0.5rem;
-    padding: 0.5rem;
-    background-color: #454d5d;
-    border-top-left-radius: 3px;
-    border-top-right-radius: 3px;
-  }
-
-  .v-step__content {
-    margin: 0 0 1rem 0;
-  }
-
-  .v-step__button {
-    background: transparent;
-    border: .05rem solid white;
-    border-radius: .1rem;
-    color: white;
-    cursor: pointer;
-    display: inline-block;
-    font-size: .8rem;
-    height: 1.8rem;
-    line-height: 1rem;
-    outline: none;
-    margin: 0 0.2rem;
-    padding: .35rem .4rem;
-    text-align: center;
-    text-decoration: none;
-    transition: all .2s ease;
-    vertical-align: middle;
-    white-space: nowrap;
-
-    &:hover {
-      background-color: rgba(white, 0.95);
-      color: #50596c;
-    }
-  }
+}
 </style>
