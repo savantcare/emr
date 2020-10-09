d5b6cf9cd4979d61199a77f3cd8452a64d191cb3
Vikas <kedia.vikas@gmail.com>
2020-10-08 23:21:00 -0700

    +read

--- webclient/components/ptinfo-combined/printable-appt-note-with-amendment-or-modify-feature/design-1.vue
+++ webclient/components/ptinfo-combined/printable-appt-note-with-amendment-or-modify-feature/design-1.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-drawer :visible.sync="drawerToShowComparisonOf2Notes" direction="ttb" size="90%">
+    <el-drawer :visible.sync="dUidrawerToShowComparisonOf2Notes" direction="ttb" size="90%">
       <el-row>
         <el-col :span="12"><apptNotePrintableView propShowNoteForApptId="1" key="2" /> </el-col
         ><el-col :span="12"><apptNotePrintableView propShowNoteForApptId="5" key="5" /></el-col>
@@ -20,14 +20,14 @@ import apptNotePrintableView from '@/components/ptinfo-combined/printable-appt-n
 export default {
   data() {
     return {
-      drawerToShowComparisonOf2Notes: false,
+      dUidrawerToShowComparisonOf2Notes: false,
     }
   },
   mounted() {
     let eventName = ['event-from-ct-note-print-view-1-show-comparison-drawer']
     // A sample event name is: 'event-from-ct-name-vl-save-this-row'
     this.$root.$on(eventName, (pRowId) => {
-      this.drawerToShowComparisonOf2Notes = true
+      this.dUidrawerToShowComparisonOf2Notes = true
     })
   },
   components: { apptNotePrintableView },
