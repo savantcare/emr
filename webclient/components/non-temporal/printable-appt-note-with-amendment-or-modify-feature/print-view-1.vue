316fce86d64a1af1a00e479d4822f8eda388ff8c
Vikas <kedia.vikas@gmail.com>
2020-10-11 21:15:14 -0700

    +read

--- webclient/components/non-temporal/printable-appt-note-with-amendment-or-modify-feature/print-view-1.vue
+++ webclient/components/non-temporal/printable-appt-note-with-amendment-or-modify-feature/print-view-1.vue
@@ -458,7 +458,7 @@ import clientSideTblOfAppointments from '@/components/1time-Mrow-mField/appointm
 import apptNotePrintableView from '@/components/non-temporal/printable-appt-note-with-amendment-or-modify-feature/print-view-1.vue'
 
 // smaller sections
-import serviceStatementPageSection from './page-section-of-service-statement.vue'
+import serviceStatementPageSection from './print-section-of-service-statement.vue'
 
 // Library
 import moment from 'moment'
