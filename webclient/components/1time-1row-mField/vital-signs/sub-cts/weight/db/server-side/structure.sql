e7802fc4f1c4ee6dfcf49614c4059bd7923c5b09
Vikas <kedia.vikas@gmail.com>
2020-10-13 16:52:20 -0700

    fixed weight value

--- webclient/components/1time-1row-mField/vital-signs/sub-cts/weight/db/server-side/structure.sql
+++ webclient/components/1time-1row-mField/vital-signs/sub-cts/weight/db/server-side/structure.sql
@@ -18,4 +18,4 @@ CREATE TABLE `weight` (
 --
 
 INSERT INTO `weight` (`serverSideRowUuid`, `ptUuid`, `weightInPounds`, `notes`, `recordChangedByUuid`, `recordChangedFromIPAddress`) VALUES
-('01817fb0-c1ef-11ea-a3a5-f36fe4d74da4', 'bfe041fa-073b-4223-8c69-0540ee678ff8', 75, 'test', 'bfe041fa-073b-4223-8c69-0540ee678ff8', NULL); 
+('01817fb0-c1ef-11ea-a3a5-f36fe4d74da4', 'bfe041fa-073b-4223-8c69-0540ee678ff8', 180, 'test', 'bfe041fa-073b-4223-8c69-0540ee678ff8', NULL); 
