6c884c4a5f8982b786b90a88399ddc733323911b
Vikas <kedia.vikas@gmail.com>
2020-10-12 22:39:02 -0700

    db init script

--- utils/db/create-db-tables-mock-data-in-db.sh
+++ utils/db/create-db-tables-mock-data-in-db.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
+
+echo "Run this from inside the DB container"
 DB_USER="root"
 
-# Find comand to run DB init scripts inside webclient/cts/
+# Find comand to run DB init scripts inside webclient/components/
 
 ## Step 1: Create DB scripts 
-for file in $(find /gt/sc-prog-repos/emr/webclient/cts/ -type f -name "create-db-*.sql" -print)
+for file in $(find /gt/sc-prog-repos/emr/webclient/components/ -type f -name "create-db*.sql" -print)
 do
     echo "== executing create DB $file";
     mysql -u $DB_USER  < $file 
 done
 
 ## Step 2: structure of tables
-for file in $(find /gt/sc-prog-repos/emr/webclient/cts/ -type f -name "structure-gen-*.sql" -print)
+for file in $(find /gt/sc-prog-repos/emr/webclient/components/ -type f -name "structure*.sql" -print)
 do
     echo "== executing create structure $file";
     mysql -u $DB_USER  < $file 
 done
 
 ## Mock data
-for file in $(find /gt/sc-prog-repos/emr/webclient/cts/ -type f -name "mock-data*.sql" -print)
+for file in $(find /gt/sc-prog-repos/emr/webclient/components/ -type f -name "mock-data*.sql" -print)
 do
     echo "== executing insert mock data $file";
     mysql -u $DB_USER  < $file 
