742a5a3a067d77c5c211ebcc7422c20405daa97f
Vikas <kedia.vikas@gmail.com>
2020-10-14 01:20:36 -0700

    zoom in chart works

--- webclient/components/non-temporal/correlate/graph-design-1.vue
+++ webclient/components/non-temporal/correlate/graph-design-1.vue
@@ -164,6 +164,7 @@ export default {
         ],
         chart: {
           width: 720, // on page load default width should be 50% of page width, ie; 700px. We have developed this software to run on 1440*900
+          zoomType: 'x',
         },
         credits: {
           enabled: false,
