fbc67efde20d2394063f4cb1efc240d8c351af29
Vikas <kedia.vikas@gmail.com>
2020-10-18 16:03:59 -0700

    +read

--- webclient/components/1time-1row-mField/past-psych-history/edit-layer/past-pysch-history-edit-design-1.vue
+++ webclient/components/1time-1row-mField/past-psych-history/edit-layer/past-pysch-history-edit-design-1.vue
@@ -21,7 +21,7 @@
                 style="width: 400px"
               ></el-input>
               <el-card style="width: 400px">
-                <div class="show-diff" v-html="diffBetweenTwoFields[ss.formFieldName]"></div>
+                <div class="show-diff" v-html="fieldDiffWithSecondayObj[ss.formFieldName]"></div>
               </el-card>
             </el-card>
           </div>
@@ -44,7 +44,7 @@ export default {
       vOrmSaveScheduledForDebounce: [],
       userTypedSearchFilterKeyword: '',
       liveTypeObjOfFields: {},
-      diffBetweenTwoFields: {},
+      fieldDiffWithSecondayObj: {},
       secondaryObjOfFieldsForComparison: [],
     }
   },
@@ -72,17 +72,17 @@ export default {
         this.debounceThenSaveToOrm('Past_outpatient_treatment', newValue, 1)
         if (this.secondaryObjOfFieldsForComparison['Past_outpatient_treatment']) {
           const diff = Diff.diffWords(this.secondaryObjOfFieldsForComparison['Past_outpatient_treatment'], newValue)
-          this.diffBetweenTwoFields['Past_outpatient_treatment'] = ''
+          this.fieldDiffWithSecondayObj['Past_outpatient_treatment'] = ''
           diff.forEach((part) => {
             // green for additions, red for deletions
             // grey for common parts
             const color = part.added ? 'green' : part.removed ? 'red' : 'grey'
-            this.diffBetweenTwoFields['Past_outpatient_treatment'] =
-              this.diffBetweenTwoFields['Past_outpatient_treatment'] + '<span style="color:' + color + '">'
-            this.diffBetweenTwoFields['Past_outpatient_treatment'] =
-              this.diffBetweenTwoFields['Past_outpatient_treatment'] + part.value
-            this.diffBetweenTwoFields['Past_outpatient_treatment'] =
-              this.diffBetweenTwoFields['Past_outpatient_treatment'] + '</span>'
+            this.fieldDiffWithSecondayObj['Past_outpatient_treatment'] =
+              this.fieldDiffWithSecondayObj['Past_outpatient_treatment'] + '<span style="color:' + color + '">'
+            this.fieldDiffWithSecondayObj['Past_outpatient_treatment'] =
+              this.fieldDiffWithSecondayObj['Past_outpatient_treatment'] + part.value
+            this.fieldDiffWithSecondayObj['Past_outpatient_treatment'] =
+              this.fieldDiffWithSecondayObj['Past_outpatient_treatment'] + '</span>'
           })
         }
       },
