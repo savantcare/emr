319d93c7e111245635e644ce4cb4b642703da533
Vikas <kedia.vikas@gmail.com>
2020-10-25 06:04:23 -0700

    +read

---
+++ webclient/components/1time-eachField-multiValues/miscellaneous-notes/edit-layer/add-ct.vue
@@ -0,0 +1,19 @@
+<template>
+  <fieldMultiValuesAdd :propCtDef="ctDef"></fieldMultiValuesAdd>
+</template>
+
+<script>
+import fieldMultiValuesAdd from '@/components/1time-eachField-multiValues/common/edit-layer/add-ct.vue'
+
+export default {
+  data: function () {
+    return {
+      ctDef: process.env.miscellaneousNotesCtDef,
+    }
+  },
+
+  components: {
+    fieldMultiValuesAdd,
+  },
+}
+</script>
