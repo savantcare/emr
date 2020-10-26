6b688a0f6412d143ecf3a918db5d1f7a97db7898
Vikas <kedia.vikas@gmail.com>
2020-10-26 16:22:38 -0700

    created common module

--- webclient/components/temporal/1-framework/edit-layer/edit-structure.vue
+++ webclient/components/temporal/1-framework/edit-layer/edit-structure.vue
@@ -90,29 +90,7 @@
   </div>
 </template>
 <script>
-import allergiesClientTbl from '@/components/temporal/allergies/db/client-side/structure/allergies-of-a-patient-table.js'
-import chiefComplaintClientTbl from '@/components/temporal/chief-complaint/db/client-side/structure/chief-complaint-of-a-patient-table.js'
-import diagnosisClientTbl from '@/components/temporal/diagnosis/db/client-side/structure/diagnosis-of-a-patient-table.js'
-import familyHistoryClientTbl from '@/components/temporal/family-history/db/client-side/structure/family-history-of-a-patient-table.js'
-import miscNotesClientTbl from '@/components/temporal/miscellaneous-notes/db/client-side/structure/misc-notes-of-a-patient-table.js'
-import planCommentsClientTbl from '@/components/temporal/plan-comments/db/client-side/structure/plan-comments-of-a-patient-table.js'
-import processNotesClientTbl from '@/components/temporal/process-notes/db/client-side/structure/process-notes-of-a-patient-table.js'
-import recommendationClientTbl from '@/components/temporal/recommendations/db/client-side/structure/recommendations-of-a-patient-table.js'
-import reminderClientTbl from '@/components/temporal/reminders/db/client-side/structure/reminders-of-a-patient-table.js'
-import serviceStatementClientTbl from '@/components/temporal/service-statements/db/client-side/structure/service-statements-of-a-patient-table.js'
-
-const clientTbl = {
-  allergies: allergiesClientTbl,
-  chief_complaint: chiefComplaintClientTbl,
-  diagnosis: diagnosisClientTbl,
-  family_history: familyHistoryClientTbl,
-  miscellaneous_notes: miscNotesClientTbl,
-  plan_comments: planCommentsClientTbl,
-  process_notes: processNotesClientTbl,
-  recommendations: recommendationClientTbl,
-  reminders: reminderClientTbl,
-  service_statements: serviceStatementClientTbl,
-}
+import clientTbl from './tables.js'
 
 export default {
   /*
--- webclient/components/temporal/service-statements/edit-layer/ss-edit-design-1.vue
+++ webclient/components/temporal/service-statements/edit-layer/ss-edit-design-1.vue
@@ -5,6 +5,7 @@
 <script>
 import ctEditStructure from '@/components/temporal/1-framework/edit-layer/edit-structure.vue'
 import clientTblOfMasterServiceStatements from '../db/client-side/structure/service-statements-master.js'
+import serviceStatementClientTbl from '@/components/temporal/service-statements/db/client-side/structure/service-statements-of-a-patient-table.js'
 
 export default {
   data: function () {
@@ -26,13 +27,16 @@ export default {
     // Inside this fn this will refer to this ct (parent) https://stackoverflow.com/questions/59826155/vue-callback-via-props-and-this
 
     this.ctDef.fnGetSelectOptions = function (fieldNameInDb) {
-      console.log('===== inside fn')
+      console.log('===== fn', fieldNameInDb)
       let arOfObjectsFromClientMasterDB = clientTblOfMasterServiceStatements
         .query()
-        .with('tblLinkToServiceStatementForPatientFieldValues')
         .where('ROW_END', 2147483648000)
         .where('serviceStatementFieldNameInDb', fieldNameInDb)
         .get()
+
+      // get the value for this field in patient table
+      const row = serviceStatementClientTbl.find(1)
+      console.log(row[fieldNameInDb])
 
       arOfObjectsFromClientMasterDB.forEach(function (data) {
         data['id'] = data['serviceStatementFieldOptionId']
