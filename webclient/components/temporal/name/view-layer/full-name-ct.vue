81196d705e10e2ca599ef09d4b46daf7b146604d
Vikas <kedia.vikas@gmail.com>
2020-10-28 00:42:00 -0700

    name add is using framework

--- webclient/components/temporal/name/view-layer/full-name-ct.vue
+++ webclient/components/temporal/name/view-layer/full-name-ct.vue
@@ -1,109 +1,18 @@
-<!-- Reference implementation for non numeric hence no graph-->
+<!-- Reference implementation -->
 <template>
-  <div>
-    <showContentInCardComponent
-      propMainCardName="Name"
-      :propActionsThatCanBeInvokedFromCardHeader="[
-        {
-          actionDescription: 'Toggle card display',
-          isDefaultAction: true,
-        },
-        {
-          actionDescription: 'Close card',
-        },
-      ]"
-      propGridDesignTopMostParentBody="s-css-class-top-most-card-body-grid"
-    >
-      <!-- Passing name of the fld so mfTypeOfButton can decide if the fld is changed or not -->
-      <div slot="bodySlotContentFromParentToShowAboveChildCards" class="box-card sc-individual-child-card">
-        <el-button-group style="float: right; display: none">
-          <el-button
-            style="padding: 3px; color: #c0c4cc; border: none"
-            plain
-            tabindex="-1"
-            @click="mfOpenEditCtInEditLayer(cfLatestDataRowFromClientTbl['clientSideUniqRowId'], 'name - edit')"
-            class="el-icon-edit"
-          />
-          <el-button
-            v-if="dataFldsOfToChangeAndCopiedRowsAreSame !== true"
-            type="success"
-            size="mini"
-            style="padding: 3px"
-            plain
-            tabindex="-1"
-            @click="mfSendReviewedEvent()"
-            >S</el-button
-          >
-          <el-button
-            v-if="dataFldsOfToChangeAndCopiedRowsAreSame !== true"
-            type="danger"
-            size="mini"
-            style="padding: 3px"
-            plain
-            tabindex="-1"
-            @click="mfSendResetFormEvent()"
-            >R</el-button
-          >
-        </el-button-group>
-        <el-button :type="mfTypeOfButton('firstName')" plain :tabindex="cfPosInArCardsInPtsOfViewLayer * 100 + 1">{{
-          cfLatestDataRowFromClientTbl['firstName']
-        }}</el-button>
-        <el-button :type="mfTypeOfButton('middleName')" plain>{{
-          cfLatestDataRowFromClientTbl['middleName']
-        }}</el-button>
-        <el-button :type="mfTypeOfButton('lastName')" plain>{{ cfLatestDataRowFromClientTbl['lastName'] }}</el-button>
-      </div>
-    </showContentInCardComponent>
-  </div>
+  <timeLineView :propFormDef="formDef"></timeLineView>
 </template>
 
 <script>
-/* Option1:
-
-import mxTable from '@/components/1time-eachField-1value/com-mx/mixins/view-layer.js'
-
-Desired. Once this works then name, height, weight can share the same mixin-view-layer.js
-But inside /com-mx/mixins/view-layer.js I am not able to import the correct
-import mxFullSyncWithDbServer from './db/full-sync-with-db-server-mixin'
-
-Since relative paths current working directory is /com-mx/
-
-Posted the question on
-stackoverflow: https://stackoverflow.com/questions/63373084/relative-path-getting-resolved-from-location-of-imported-file
-Discord: https://discord.com/channels/325477692906536972/325479107012067328/743027109659672616
-
-*/
-
-/* option 2:
-const mxTable = require('../code-common-for-all-1r-mf/view-layer.js').default         -> Works
-Why use this?
-Require allows to pass variable names Ref: https://stackoverflow.com/questions/13151693/passing-arguments-to-require-when-loading-module
-Problem:
-const mxTable = require('../code-common-for-all-1r-mf/view-layer.js')('weight').default -> Does not work
-*/
-
-/* Option3: Working. But in this option the same file '../code-common-for-all-1r-mf/view-layer.js' has to be kept in each folder like height weight name */
-import mxViewLayer from '../code-common-for-all-1r-mf/view-layer.js'
-import showContentInCardComponent from '@/components/non-temporal/display-manager/show-content-in-card-component.vue'
+import timeLineView from '@/components/temporal/1-framework/view-layer/timeline-structure.vue'
+import { nameFormDef } from '@/components/temporal/name/db/client-side/structure/name-of-a-patient-table.js'
 
 export default {
-  components: { showContentInCardComponent },
-  mixins: [mxViewLayer],
-  computed: {
-    cfArOfNameForDisplay() {
-      let arOfObjectsFromClientDB = new Array()
-      let obj = new Object()
-      obj['cardContentOfTypeStringToShowInBodyOfCards'] =
-        this.cfLatestDataRowFromClientTbl['firstName'] +
-        ' - ' +
-        this.cfLatestDataRowFromClientTbl['middleName'] +
-        ' - ' +
-        this.cfLatestDataRowFromClientTbl['lastName']
-
-      obj['clientSideUniqRowId'] = this.cfLatestDataRowFromClientTbl['clientSideUniqRowId']
-      arOfObjectsFromClientDB.push(obj)
-      return arOfObjectsFromClientDB
-    },
+  data: function () {
+    return {
+      formDef: nameFormDef,
+    }
   },
+  components: { timeLineView },
 }
 </script>
