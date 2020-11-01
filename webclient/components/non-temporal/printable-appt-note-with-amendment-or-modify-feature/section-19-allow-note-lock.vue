24df6c64680a16a679e2836938c8e27c1dbe58a6
Vikas <kedia.vikas@gmail.com>
2020-10-31 22:05:50 -0700

    Framework to save data

--- webclient/components/non-temporal/printable-appt-note-with-amendment-or-modify-feature/section-19-allow-note-lock.vue
+++ webclient/components/non-temporal/printable-appt-note-with-amendment-or-modify-feature/section-19-allow-note-lock.vue
@@ -11,6 +11,7 @@
 <script>
 import clientTblOfAppointments from '@/components/temporal/appointments/db/client-side/structure/appointment-client-side-table.js'
 import clientTblOfLeftSideViewCards from '@/components/non-temporal/components-container-in-lhs-of-layer1/db/client-side/structure/left-hand-side-table-of-cards.js'
+import allClientTbls from '@/components/framework/all-client-tables.js'
 
 export default {
   data() {
@@ -42,12 +43,27 @@ export default {
   methods: {
     async lockButtonClicked() {
       console.log('lock button clicked')
+
+      /* Find rows in chief complaints and then make API calls. All rowStatus begining with 2 will be post and 
+                                                                All rowStatus begining with 3 will be patch
+      Loop through all the tables 
+      */
+      for (const property in allClientTbls) {
+        // console.log(`${property}: ${allClientTbls[property]}`)
+        const newRowsToSave = allClientTbls[property].fnGetNewRowsInEditState()
+        for (let i = 0; i < newRowsToSave.length; i++) {
+          console.log('Make post request to', allClientTbls[property].apiUrl)
+          console.log(newRowsToSave[i])
+        }
+      }
+      // console.log(allClientTbls)
+
       const clientSideUniqRowId = this.currentApptObj['clientSideUniqRowId']
       let arOfObjectsFromClientDB = await clientTblOfAppointments.update({
         where: clientSideUniqRowId,
         data: {
           apptStatus: 'locked',
-          ROW_END: Math.floor(Date.now()), // The query sent to server is "delete"
+          ROW_END: Math.floor(Date.now()), // The query sent to server is "delete". When a appt is locked then we delete that from the appt table.
         },
       })
 
