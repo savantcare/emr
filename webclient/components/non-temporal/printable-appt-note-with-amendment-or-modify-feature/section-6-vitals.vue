9e716d5ed7a911eb5bfe25d48026f0ff995870a2
Vikas <kedia.vikas@gmail.com>
2020-11-01 05:57:07 -0800

    ht and wt comes in view layer

--- webclient/components/non-temporal/printable-appt-note-with-amendment-or-modify-feature/section-6-vitals.vue
+++ webclient/components/non-temporal/printable-appt-note-with-amendment-or-modify-feature/section-6-vitals.vue
@@ -1,1 +1,30 @@
-<template></template>
+<template>
+  <div>
+    <printNote :propApptId="propApptId" :propFormDef="weightFormDef"></printNote>
+    <printNote :propApptId="propApptId" :propFormDef="heightFormDef"></printNote>
+  </div>
+</template>
+
+<script>
+import printNote from '@/components/def-processors/view/paper.vue'
+import { weightFormDef } from '@/components/temporal/weight/db/client-side/structure/weight-of-a-patient-table.js'
+import { heightFormDef } from '@/components/temporal/height/db/client-side/structure/height-of-a-patient-table.js'
+
+export default {
+  components: {
+    printNote,
+  },
+  props: {
+    propApptId: {
+      type: Number,
+      required: true,
+    },
+  },
+  data: function () {
+    return {
+      weightFormDef: weightFormDef,
+      heightFormDef: heightFormDef,
+    }
+  },
+}
+</script>
