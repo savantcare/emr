67c1c1372f20be840a5fe8a47783d6a37c013fd8
Raj Roushan Jha <raj@grmtech.com>
2020-11-03 18:31:32 +0530

    Now BMI saved to database

--- servers/lumen/routes/web.php
+++ servers/lumen/routes/web.php
@@ -243,4 +243,20 @@ $router->group(['prefix' => 'api'], function () use ($router) {
             ->header('Access-Control-Allow-Credentials', 'true')
             ->header('Connection', 'keep-alive');
     });
+
+    // BMI
+    $router->get('bmi/v20/', ['uses' => 'BmiController@getAllTemporalBmis']);
+    $router->get('bmi/v20/{pServerSideRowUuid}', ['uses' => 'BmiController@getOneBmi']);
+    $router->post('bmi/v20/', ['uses' => 'BmiController@create']);
+    $router->put('bmi/v20/{pServerSideRowUuid}', ['uses' => 'BmiController@update']);
+    $router->options('bmi/v20', function () {
+        return response('OK', \Illuminate\Http\Response::HTTP_NO_CONTENT)
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Connection', 'keep-alive');
+    });
+    $router->options('bmi/v20/{pServerSideRowUuid}', function () {
+        return response('OK', \Illuminate\Http\Response::HTTP_NO_CONTENT)
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Connection', 'keep-alive');
+    });
 });
