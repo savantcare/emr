30e6df75629b7193892f44a4c6dd8783f7a25eac
Mithun Kayal <mithun@grmtech.com>
2020-11-05 13:16:13 +0530

    Update web.php
    change conflict file

--- servers/lumen/routes/web.php
+++ servers/lumen/routes/web.php
@@ -271,13 +271,13 @@ $router->group(['prefix' => 'api'], function () use ($router) {
             ->header('Access-Control-Allow-Credentials', 'true')
             ->header('Connection', 'keep-alive');
     });
-    $router->options('diagnosis/v20/{serverSideRowUuid}', function () {
     $router->options('plan-comments/v20/{pServerSideRowUuid}', function () {
         return response('OK', \Illuminate\Http\Response::HTTP_NO_CONTENT)
             ->header('Access-Control-Allow-Credentials', 'true')
             ->header('Connection', 'keep-alive');
     });
-      
+    
+    // Diagnosis  
     $router->get('diagnosis/v20/', ['uses' => 'DiagnosisController@getAllTemporalDiagnosis']);
     $router->post('diagnosis/v20/', ['uses' => 'DiagnosisController@create']);
     $router->delete('diagnosis/v20/{serverSideRowUuid}', ['uses' => 'DiagnosisController@delete']);
