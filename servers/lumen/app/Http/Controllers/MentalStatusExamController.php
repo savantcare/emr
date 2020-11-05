528ff9c6ed1a3be26b0773ca125cb5f7784bfcf6
sanjay-grmtech <sanjaysaha@grmtech.com>
2020-11-05 17:14:01 +0530

    add update function

--- servers/lumen/app/Http/Controllers/MentalStatusExamController.php
+++ servers/lumen/app/Http/Controllers/MentalStatusExamController.php
@@ -22,20 +22,29 @@ class MentalStatusExamController extends Controller
     public function create(Request $pRequest)
     {
         $requestData = $pRequest->all();
-        
+        $recordChangedFromIPAddress = $this->get_client_ip();
         $arMentalStatusExamData = array(
             'serverSideRowUuid' => $requestData['data']['serverSideRowUuid'],
-            'patientUuid' => $requestData['data']['ptUuid'],
+            'ptUuid' => $requestData['data']['ptUuid'],
             'appearance_select' => $requestData['data']['appearance_select'],
             'psychomotor_select' => $requestData['data']['psychomotor_select'],
             'attitude_multi_select' => $requestData['data']['attitude_multi_select'],
             'recordChangedByUuid' => $requestData['data']['recordChangedByUuid'],
-            'recordChangedFromIPAddress' => $requestData['data']['recordChangedFromIPAddress'],
+            'recordChangedFromIPAddress' => $recordChangedFromIPAddress
         );
        
         $mentalStatusExam = MentalStatusExam::insertGetId($arMentalStatusExamData);
 
         return response()->json($mentalStatusExam, 201);
+    }
+
+    public function update($pServerSideRowUuid, Request $pRequest)
+    {
+        $requestData = $pRequest->all();
+        $MentalStatusExam = ServiceStatement::findOrFail($pServerSideRowUuid);
+        $MentalStatusExam->update($requestData['data']);
+
+        return response()->json($MentalStatusExam, 200);
     }
  
     public function delete($pServerSideRowUuid)
@@ -43,5 +52,24 @@ class MentalStatusExamController extends Controller
         $mentalStatusExam = MentalStatusExam::findOrFail($pServerSideRowUuid);
         $mentalStatusExam->delete();
         return response('Deleted successfully', 200);
+    }
+
+    public function get_client_ip() {
+        $ipaddress = '';
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if(isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
     }
 }
