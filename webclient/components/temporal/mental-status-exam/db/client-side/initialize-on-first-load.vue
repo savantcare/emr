4980f045f76997ec5229bfa3a75e45bff6825239
ss <ss@166desktop.local>
2020-11-09 15:25:12 +0530

    improve mse variable name on initial load file

--- webclient/components/temporal/mental-status-exam/db/client-side/initialize-on-first-load.vue
+++ webclient/components/temporal/mental-status-exam/db/client-side/initialize-on-first-load.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <step1_init_search_phrases />
-    <step2_init_mse_field_names />
+    <step2_init_mse_select_options />
   </div>
 </template>
 
 <script>
 import step1_init_search_phrases from '@/components/temporal/mental-status-exam/db/client-side/static-data/insert-into-master-of-search-phrases-ct.vue'
-import step2_init_mse_field_names from '@/components/temporal/mental-status-exam/db/client-side/static-data/insert-into-mental-status-exam-all-select-options.vue'
+import step2_init_mse_select_options from '@/components/temporal/mental-status-exam/db/client-side/static-data/insert-into-mental-status-exam-all-select-options.vue'
 
 import clientTbl from '~/components/temporal/mental-status-exam/db/client-side/structure/mental-status-exam-of-a-patient-table.js'
 // import step2_init_mse_field_names from '@/components/temporal/mental-status-exam/db/client-side/dynamic-data/get-initial-data-from-server-ct.vue'
@@ -15,7 +15,7 @@ import clientTbl from '~/components/temporal/mental-status-exam/db/client-side/s
 export default {
   components: {
     step1_init_search_phrases,
-    step2_init_mse_field_names,
+    step2_init_mse_select_options,
   },
   async mounted() {
     /*
