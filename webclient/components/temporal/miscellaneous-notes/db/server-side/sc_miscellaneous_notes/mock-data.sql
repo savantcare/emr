6daa1f45922ad2d4294fa85706e2b9e2084534e3
shubhadeep-grmtech <shubhadeep@grmtech.com>
2020-11-11 17:58:41 +0530

    miscellaneous notes data save to database

--- webclient/components/temporal/miscellaneous-notes/db/server-side/sc_miscellaneous_notes/mock-data.sql
+++ webclient/components/temporal/miscellaneous-notes/db/server-side/sc_miscellaneous_notes/mock-data.sql
@@ -1,6 +1,6 @@
 use sc_misc_notes;
 
 INSERT INTO `misc-notes` (`serverSideRowUuid`, `ptUuid`, `description`, `notes`, `priority`, `isAutoRem`, `recordChangedByUuid`, `recordChangedFromIPAddress`, `recordChangedFromSection`) VALUES
-('01817fb0-c1ef-11ea-a3a5-f36fe4d74da4', 'bfe041fa-073b-4223-8c69-0540ee678ff8', 'Jai kali ma', NULL, 1, 1, 'bfe041fa-073b-4223-8c69-0540ee678ff8', NULL, 'patientFile'),
-('084e11a0-c1ef-11ea-a3a5-f36fe4d74da4', 'bfe041fa-073b-4223-8c69-0540ee678ff8', 'Jai durga ma', NULL, 1, 1, 'bfe041fa-073b-4223-8c69-0540ee678ff8', NULL, 'patientFile');
+('01817fb0-c1ef-11ea-a3a5-f36fe4d74da4', 'bfe041fa-073b-4223-8c69-0540ee678ff8', 'Jai kali ma', NULL, 'bfe041fa-073b-4223-8c69-0540ee678ff8', NULL, 'patientFile'),
+('084e11a0-c1ef-11ea-a3a5-f36fe4d74da4', 'bfe041fa-073b-4223-8c69-0540ee678ff8', 'Jai durga ma', NULL, 'bfe041fa-073b-4223-8c69-0540ee678ff8', NULL, 'patientFile');
 
