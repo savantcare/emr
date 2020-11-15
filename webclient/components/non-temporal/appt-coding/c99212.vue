8057e37577edb310f5a960c46f1364cd61c97e67
Vikas <kedia.vikas@gmail.com>
2020-11-15 14:21:21 -0800

    +ui

--- webclient/components/non-temporal/appt-coding/c99212.vue
+++ webclient/components/non-temporal/appt-coding/c99212.vue
@@ -1,41 +1,37 @@
 <template>
   <div>
     <el-card id="thresolds" :style="status || this.features.showDetails ? '' : 'display:none'">
-      <div style="display: grid; grid-template-columns: 1ft; align-items: start">
-        <div style="border-style: dashed; border-width: 1px">
-          <span :class="status ? '' : 'cross'">
-            99212
-            <transition name="fade">
-              <span style="color: green; font-weight: bold" v-if="status">&check; </span>
-            </transition>
-          </span>
-        </div>
+      <span :class="status ? '' : 'cross'">
+        99212
+        <transition name="fade">
+          <span style="color: green; font-weight: bold" v-if="status">&check; </span>
+        </transition>
+      </span>
 
-        <div style="text-align: center">
-          <span :class="statusC3 ? '' : 'strike'" style="border-style: dashed; border-width: 1px">
-            3. Billing duration: Greater then 9 min
-            <transition name="fade">
-              <span v-if="statusC3" style="color: green; font-weight: bold"> &check;</span></transition
-            >
-          </span>
-        </div>
+      <div style="text-align: center">
+        <span :class="statusC3 ? '' : 'strike'" style="border-style: dashed; border-width: 1px">
+          3. Billing duration: Greater then 9 min
+          <transition name="fade">
+            <span v-if="statusC3" style="color: green; font-weight: bold"> &check;</span></transition
+          >
+        </span>
+      </div>
 
-        <div style="text-align: center">
-          <span :class="statusC2 ? '' : 'strike'" style="border-style: dashed; border-width: 1px">
-            2. This is followup appt
-            <transition name="fade">
-              <span v-if="statusC2" style="color: green; font-weight: bold"> &check;</span></transition
-            >
-          </span>
-        </div>
-        <div style="text-align: center">
-          <span :class="statusC1 ? '' : 'strike'" style="border-style: dashed; border-width: 1px">
-            1. Medical appt
-            <transition name="fade">
-              <span v-if="statusC1" style="color: green; font-weight: bold"> &check;</span></transition
-            >
-          </span>
-        </div>
+      <div style="text-align: center">
+        <span :class="statusC2 ? '' : 'strike'" style="border-style: dashed; border-width: 1px">
+          2. This is followup appt
+          <transition name="fade">
+            <span v-if="statusC2" style="color: green; font-weight: bold"> &check;</span></transition
+          >
+        </span>
+      </div>
+      <div style="text-align: center">
+        <span :class="statusC1 ? '' : 'strike'" style="border-style: dashed; border-width: 1px">
+          1. Medical appt
+          <transition name="fade">
+            <span v-if="statusC1" style="color: green; font-weight: bold"> &check;</span></transition
+          >
+        </span>
       </div>
     </el-card>
   </div>
