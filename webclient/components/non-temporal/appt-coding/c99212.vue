14a19ebfdfd7639848ba4ea5785931400aef1e50
Vikas <kedia.vikas@gmail.com>
2020-11-15 14:10:57 -0800

    +ui

--- webclient/components/non-temporal/appt-coding/c99212.vue
+++ webclient/components/non-temporal/appt-coding/c99212.vue
@@ -1,37 +1,42 @@
 <template>
   <div>
-    <el-card id="thresolds" :style="status || this.features.showDetails ? '' : 'display:none'"
-      ><span :class="status ? '' : 'cross'">
-        99212
-        <transition name="fade">
-          <span style="color: green; font-weight: bold" v-if="status">&check; </span>
-        </transition>
-      </span>
-      <br />
+    <el-card id="thresolds" :style="status || this.features.showDetails ? '' : 'display:none'">
+      <div style="display: grid; grid-template-columns: 1ft; align-items: start">
+        <div style="border-style: dashed; border-width: 1px">
+          <span :class="status ? '' : 'cross'">
+            99212
+            <transition name="fade">
+              <span style="color: green; font-weight: bold" v-if="status">&check; </span>
+            </transition>
+          </span>
+        </div>
 
-      <span :class="statusC1 ? '' : 'strike'">
-        1. Medical appt
-        <transition name="fade">
-          <span v-if="statusC1" style="color: green; font-weight: bold"> &check;</span></transition
-        >
-      </span>
-      <br />
+        <div style="border-style: dashed; border-width: 1px">
+          <span :class="statusC3 ? '' : 'strike'">
+            3. Billing duration: Greater then 9 min
+            <transition name="fade">
+              <span v-if="statusC3" style="color: green; font-weight: bold"> &check;</span></transition
+            >
+          </span>
+        </div>
 
-      <span :class="statusC2 ? '' : 'strike'">
-        2. This is followup appt
-        <transition name="fade">
-          <span v-if="statusC2" style="color: green; font-weight: bold"> &check;</span></transition
-        >
-      </span>
-      <br />
-
-      <span :class="statusC3 ? '' : 'strike'">
-        3. Billing duration: Greater then 9 min
-        <transition name="fade">
-          <span v-if="statusC3" style="color: green; font-weight: bold"> &check;</span></transition
-        >
-      </span>
-      <br />
+        <div style="border-style: dashed; border-width: 1px">
+          <span :class="statusC2 ? '' : 'strike'">
+            2. This is followup appt
+            <transition name="fade">
+              <span v-if="statusC2" style="color: green; font-weight: bold"> &check;</span></transition
+            >
+          </span>
+        </div>
+        <div style="border-style: dashed; border-width: 1px">
+          <span :class="statusC1 ? '' : 'strike'">
+            1. Medical appt
+            <transition name="fade">
+              <span v-if="statusC1" style="color: green; font-weight: bold"> &check;</span></transition
+            >
+          </span>
+        </div>
+      </div>
     </el-card>
   </div>
 </template>
---
+++ webclient/components/non-temporal/appt-coding/readme.md
@@ -0,0 +1,3 @@
+Fill up like a water bottle
+Lower to higher numbers like steps achievement
+if not intake then intake card shold go to bottom
