84f60e3a88b396d86dee9519904a3c92c5e88378
Vikas <kedia.vikas@gmail.com>
2020-11-14 16:39:52 -0800

    more cards appear in the card component

--- webclient/components/non-temporal/coding/dashboard.vue
+++ webclient/components/non-temporal/coding/dashboard.vue
@@ -2,13 +2,38 @@
   <div>
     <button class="el-icon-setting" @click="handleClickOnSettingsIcon"></button>
     <el-dialog title="" :visible.sync="dIsSettingsDialogVisible" width="30%">
-      <code90834 />
+      <c90832 />
+      <c90834 />
+      <c90837 />
+      <c99201 />
+      <c99202 />
+      <c99203 />
+      <c99204 />
+      <c99205 />
+      <c99211 />
+      <c99212 />
+      <c99213 />
+      <c99214 />
+      <c99215 />
     </el-dialog>
   </div>
 </template>
 
 <script>
-import code90834 from './c90834.vue'
+import c90832 from './c90832.vue'
+import c90834 from './c90834.vue'
+import c90837 from './c90837.vue'
+
+import c99201 from './c99201.vue'
+import c99202 from './c99202.vue'
+import c99203 from './c99203.vue'
+import c99204 from './c99204.vue'
+import c99205 from './c99205.vue'
+import c99211 from './c99211.vue'
+import c99212 from './c99212.vue'
+import c99213 from './c99213.vue'
+import c99214 from './c99214.vue'
+import c99215 from './c99215.vue'
 
 export default {
   data() {
@@ -16,10 +41,22 @@ export default {
       dIsSettingsDialogVisible: false,
     }
   },
-  components: { code90834 },
-  async mounted() {
-    console.log(ctAccord)
+  components: {
+    c90832,
+    c90834,
+    c90837,
+    c99201,
+    c99202,
+    c99203,
+    c99204,
+    c99205,
+    c99211,
+    c99212,
+    c99213,
+    c99214,
+    c99215,
   },
+  async mounted() {},
   methods: {
     handleClickOnSettingsIcon() {
       console.log('setting to true')
