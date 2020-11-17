02cc2215508547a9c0081970c34d547ec4e46866
Vikas <kedia.vikas@gmail.com>
2020-11-17 15:32:36 -0800

    +read

--- webclient/components/non-temporal/appt-coding-v2/dashboard.vue
+++ webclient/components/non-temporal/appt-coding-v2/dashboard.vue
@@ -92,51 +92,19 @@ import history from './history.vue'
 import examination from './examination.vue'
 import medDecisonMagking from './med-decision-making.vue'
 
-import c90832 from './c90832.vue'
-import c90834 from './c90834.vue'
-import c90837 from './c90837.vue'
-
-import c99201 from './c99201.vue'
-import c99202 from './c99202.vue'
-import c99203 from './c99203.vue'
-import c99204 from './c99204.vue'
-import c99205 from './c99205.vue'
-import c99211 from './c99211.vue'
-import c99212 from './c99212.vue'
-import c99213 from './c99213.vue'
-import c99214 from './c99214.vue'
-import c99215 from './c99215.vue'
-import MedDecisionMaking from './med-decision-making.vue'
-import Examination from './examination.vue'
-
 export default {
   data() {
     return {
       dIsSettingsDialogVisible: false,
       features: {
         billingDuration: 36,
-        pphx: false,
-        shx: false,
-        fhx: false,
       },
     }
   },
   components: {
-    c90832,
-    c90834,
-    c90837,
-    c99201,
-    c99202,
-    c99203,
-    c99204,
-    c99205,
-    c99211,
-    c99212,
-    c99213,
-    c99214,
-    c99215,
-    MedDecisionMaking,
-    Examination,
+    history,
+    medDecisionMaking,
+    examination,
   },
   async mounted() {},
   methods: {
