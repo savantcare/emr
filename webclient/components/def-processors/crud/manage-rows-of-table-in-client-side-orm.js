ee69a687efd7ff929edde8922fbe1b4e1944bbb9
Raj Roushan Jha <raj@grmtech.com>
2020-11-17 17:02:02 +0530

    Fixed the issue - Father added on Oct3rd. Mother added on Nov 11th. Mother also shown on Oct3rd slide.

--- webclient/components/def-processors/crud/manage-rows-of-table-in-client-side-orm.js
+++ webclient/components/def-processors/crud/manage-rows-of-table-in-client-side-orm.js
@@ -613,6 +613,27 @@ Decision: We will make arOrmRowsCached as a 3D array. Where the 1st D will be en
         vnRowStateInSession: pRowStatus,
         isValidationError: false,
       }
+
+      /**
+       * Q: Why I am updating ROW_START?
+       * -- Problem statement:
+       * After adding something in paper view and then locking note does following in ORM in the respective manner:
+       * 1. Saves the respective row
+       * 2. Inserts a blank row
+       * 3. And then locks the note
+       *
+       * ROW_START value of this blank row is older than the note lock time.
+       * Hence, after note lock if someone tries to add another value (in unlocked note) then ORM updates that blank row (stated in above pt-2)
+       * This is causing unexpected behaviour since ROW_START value of that blank row is older than the note lock time stated in above pt-3
+       * In other words ROW_START value of that blank row supposed to be a time after the note lock time.
+       *
+       * -- Solution:
+       * I am updating ROW_START value of row whenever user starts typing on it.
+       */
+      const currentValue = this.find(pClientRowId)
+      if (currentValue[pFldName] === '') {
+        row.ROW_START = Math.floor(Date.now())
+      }
     }
     const arFromClientTbl = this.update({
       where: pClientRowId,
