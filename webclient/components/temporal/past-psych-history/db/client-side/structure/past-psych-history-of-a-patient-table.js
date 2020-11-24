f1196bb3c3bdbc0ebff3e28a880682868f2410dc
Vikas <kedia.vikas@gmail.com>
2020-11-24 04:08:51 -0800

    +

--- webclient/components/temporal/past-psych-history/db/client-side/structure/past-psych-history-of-a-patient-table.js
+++ webclient/components/temporal/past-psych-history/db/client-side/structure/past-psych-history-of-a-patient-table.js
@@ -59,7 +59,7 @@ export const pastPsychHistoryFormDef = {
   plural: 'past psych history',
   singular: 'past psych history',
   styleForEachRowInAddForm:
-    'padding: 0px; margin: 0px; display: grid; grid-template-columns: 4fr 1fr; grid-column-gap: 1rem',
+    'padding: 0px; margin: 0px; display: grid; grid-template-columns: 1fr; grid-column-gap: 1rem; grid-row-gap: 1rem',
   fieldsDef: [
     {
       fieldNameInDb: 'past_outpatient_treatment',
@@ -134,6 +134,4 @@ export const pastPsychHistoryFormDef = {
     // it is critical that empty array is returned. Since v-model uses it. And validation uses v-model
     return []
   },
-  styleForEachRowInAddForm:
-    'padding: 0px; margin: 0px; display: grid; grid-template-columns: 1fr 1fr 1fr; grid-column-gap: 1rem',
 }
