14f184752475e1c9a16255bb22ba13a68dc4cb17
Vikas <kedia.vikas@gmail.com>
2020-11-29 19:54:55 -0800

    +

--- webclient/components/patient-data/medical-review-of-system/db/client-side/static-data/insert-into-medical-review-of-system-all-select-options.vue
+++ webclient/components/patient-data/medical-review-of-system/db/client-side/static-data/insert-into-medical-review-of-system-all-select-options.vue
@@ -12,77 +12,33 @@ export default {
     return {}
   },
   mounted() {
-    let i = 1
-    medicalReviewOfSystemAllSelectOptionsTbl.insert({
-      data: {
-        fieldOptionId: i + '#', // # is the seperator charecter so toggle can work. Look inside manage-rows
-        fieldOptionLabel: 'Change in appetite',
-        fieldNameInDb: 'recently_noticed_select',
-      },
-    })
-
-    i = i + 1
-    medicalReviewOfSystemAllSelectOptionsTbl.insert({
-      data: {
-        fieldOptionId: i + '#',
-        fieldOptionLabel: 'Fatigue',
-        fieldNameInDb: 'recently_noticed_select',
-      },
-    })
-
-    i = i + 1
-    medicalReviewOfSystemAllSelectOptionsTbl.insert({
-      data: {
-        fieldOptionId: i + '#',
-        fieldOptionLabel: 'Change in weight',
-        fieldNameInDb: 'recently_noticed_select',
-      },
-    })
-
-    i = i + 1
-    medicalReviewOfSystemAllSelectOptionsTbl.insert({
-      data: {
-        fieldOptionId: i + '#',
-        fieldOptionLabel: 'None',
-        fieldNameInDb: 'recently_noticed_select',
-      },
-    })
-
-    i = i + 1
-    medicalReviewOfSystemAllSelectOptionsTbl.insert({
-      data: {
-        fieldOptionId: i + '#',
-        fieldOptionLabel: 'Night sweats',
-        fieldNameInDb: 'recently_noticed_select',
-      },
-    })
-
-    i = i + 1
-    medicalReviewOfSystemAllSelectOptionsTbl.insert({
-      data: {
-        fieldOptionId: i + '#',
-        fieldOptionLabel: 'Difficulty sleeping',
-        fieldNameInDb: 'constitutional_systems_select',
-      },
-    })
-
-    i = i + 1
-    medicalReviewOfSystemAllSelectOptionsTbl.insert({
-      data: {
-        fieldOptionId: i + '#',
-        fieldOptionLabel: 'Feeling hotter than everyone',
-        fieldNameInDb: 'constitutional_systems_select',
-      },
-    })
-
-    i = i + 1
-    medicalReviewOfSystemAllSelectOptionsTbl.insert({
-      data: {
-        fieldOptionId: i + '#',
-        fieldOptionLabel: 'Sores that dont heal',
-        fieldNameInDb: 'skin_conditions_select',
-      },
-    })
+    const selectOptions = {
+      recently_noticed_select: [
+        { label: 'Change in appetite' },
+        { label: 'Fatigue' },
+        { label: 'Change in weight' },
+        { label: 'None' },
+        { label: 'Night sweats' },
+      ],
+      constitutional_systems_select: [{ label: 'Difficulty sleeping' }, { label: 'Feeling hotter than everyone' }],
+      skin_conditions_select: [{ label: 'Sores that dont heal' }],
+    }
+    let rowUniqId = 0
+    let fldName = {}
+    for (fldName in selectOptions) {
+      const obj = selectOptions[fldName]
+      for (let i = 0; i < obj.length; i++) {
+        const label = obj[i].label
+        medicalReviewOfSystemAllSelectOptionsTbl.insert({
+          data: {
+            fieldOptionId: '#' + rowUniqId + '#', // # is the seperator charecter so toggle can work. Look inside manage-rows
+            fieldOptionLabel: label,
+            fieldNameInDb: fldName,
+          },
+        })
+        rowUniqId++
+      }
+    }
   },
 }
 </script>
