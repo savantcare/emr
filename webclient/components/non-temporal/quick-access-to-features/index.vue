028ca59f35c5bb71408bc1124e7790f9ffb41396
Vikas <kedia.vikas@gmail.com>
2020-12-01 14:32:35 -0800

    +r

--- webclient/components/non-temporal/quick-access-to-features/index.vue
+++ webclient/components/non-temporal/quick-access-to-features/index.vue
@@ -1,16 +1,5 @@
 <template>
-  <div>
-    <!-- Goal: Implement "system preferences -> Mission control -> hot corners" concept of MacOS on the view area -->
-    <span
-      @mouseover="actOnMouseOverSpan('analysis')"
-      style="position: absolute; width: 4px; height: 50px; background-color: blue"
-    ></span>
-    <span
-      @mouseover="actOnMouseOverSpan('production')"
-      style="position: absolute; width: 4px; height: 50px; background-color: green; right: 0"
-    ></span>
-    <!-- END of hot corner area defination -->
-  </div>
+  <div></div>
 </template>
 
 <script>
@@ -26,6 +15,7 @@ export default {
   mounted() {
     // Goal: Open different "patient data sections" in the change layer with KB shortcuts-->
 
+    // Overwriting the Stopcallback fn so by default all shortcuts will work in input area. Ref: https://craig.is/killing/mice
     this.$mousetrap.prototype.stopCallback = function (e, element, combo) {
       return false
     }
@@ -34,80 +24,61 @@ export default {
     var parentScope = this
 
     this.$mousetrap.bind(['a', 'ctrl+a'], function (e) {
-      parentScope.activateTab(e, 'allergies')
-      return false // without this when the allergies form opens it will have a written in it
+      return parentScope.activateTab(e, 'allergies') // if actuvateTab returns true then the charecter shows in the form. Otherwise the charecter does not show in form
     })
     this.$mousetrap.bind(['c', 'ctrl+c'], function (e) {
-      parentScope.activateTab(e, 'chief_complaint')
-      return false
+      return parentScope.activateTab(e, 'chief_complaint')
     })
     this.$mousetrap.bind(['d', 'ctrl+d'], function (e) {
-      parentScope.activateTab(e, 'diagnosis')
-      return false
+      return parentScope.activateTab(e, 'diagnosis')
     })
     this.$mousetrap.bind(['e', 'ctrl+e'], function (e) {
-      parentScope.activateTab(e, 'examination')
-      return false
+      return parentScope.activateTab(e, 'examination')
     })
     this.$mousetrap.bind(['f', 'ctrl+f'], function (e) {
-      parentScope.activateTab(e, 'family_history')
-      return false
+      return parentScope.activateTab(e, 'family_history')
     })
     this.$mousetrap.bind(['g', 'ctrl+g'], function (e) {
-      parentScope.activateTab(e, 'goals')
-      return false
+      return parentScope.activateTab(e, 'goals')
     })
     this.$mousetrap.bind(['h', 'ctrl+h'], function (e) {
-      parentScope.activateTab(e, 'psych_review_of_system')
-      return false
+      return parentScope.activateTab(e, 'psych_review_of_system')
     })
     this.$mousetrap.bind(['i', 'ctrl+i'], function (e) {
-      parentScope.activateTab(e, 'reminders')
-      return false
+      return parentScope.activateTab(e, 'reminders')
     })
     this.$mousetrap.bind(['l', 'ctrl+l'], function (e) {
-      parentScope.activateTab(e, 'plan_comments')
-      return false
+      return parentScope.activateTab(e, 'plan_comments')
     })
     this.$mousetrap.bind(['m', 'ctrl+w'], function (e) {
-      parentScope.activateTab(e, 'medication_orders')
-      return false
+      return parentScope.activateTab(e, 'medication_orders')
     })
     this.$mousetrap.bind(['n', 'ctrl+n'], function (e) {
-      parentScope.activateTab(e, 'miscellaneous_notes')
-      return false
+      return parentScope.activateTab(e, 'miscellaneous_notes')
     })
     this.$mousetrap.bind(['o', 'ctrl+o'], function (e) {
-      parentScope.activateTab(e, 'process_notes')
-      return false
+      return parentScope.activateTab(e, 'process_notes')
     })
     this.$mousetrap.bind(['p', 'ctrl+p'], function (e) {
-      parentScope.activateTab(e, 'past_psych_history')
-      return false
+      return parentScope.activateTab(e, 'past_psych_history')
     })
     this.$mousetrap.bind(['r', 'ctrl+r'], function (e) {
-      parentScope.activateTab(e, 'screens')
-      return false
+      return parentScope.activateTab(e, 'screens')
     })
     this.$mousetrap.bind(['s', 'ctrl+s'], function (e) {
-      parentScope.activateTab(e, 'service_statements')
-      return false
+      return parentScope.activateTab(e, 'service_statements')
     })
     this.$mousetrap.bind(['t', 'ctrl+t'], function (e) {
-      parentScope.activateTab(e, 'recommendations')
-      return false
+      return parentScope.activateTab(e, 'recommendations')
     })
     this.$mousetrap.bind(['v', 'ctrl+v'], function (e) {
-      parentScope.activateTab(e, 'vitals')
-      return false
+      return parentScope.activateTab(e, 'vitals')
     })
     this.$mousetrap.bind(['w', 'ctrl+w'], function (e) {
-      parentScope.activateTab(e, 'medical_review_of_system')
-      return false
+      return parentScope.activateTab(e, 'medical_review_of_system')
     })
     this.$mousetrap.bind(['y', 'ctrl+f'], function (e) {
-      parentScope.activateTab(e, 'medical_history')
-      return false
+      return parentScope.activateTab(e, 'medical_history')
     })
 
     // Open dot phrases
@@ -115,21 +86,22 @@ export default {
   },
   methods: {
     activateTab(e, pTab) {
-      //      debugger
+      // This if is needed so that ".a" and any such pattern will work from input and textarea.
       if (
         e.srcElement.tagName == 'INPUT' ||
         e.srcElement.tagName == 'SELECT' ||
         e.srcElement.tagName == 'TEXTAREA' ||
         (e.srcElement.contentEditable && e.srcElement.contentEditable == 'true')
       ) {
-        console.log('this shortcut not allowed here')
+        // case: Shortcut not allowed here
+        return true // If user pressed a .. this a will be visisble since I am returning true here
       } else {
         clientTblOfCommonForAllComponents.insertOrUpdate({
           data: [{ fieldName: 'form-def-id-for-change-in-vertical-tabs', fieldValue: pTab }],
         })
+        // Goal: Do not see C typed in the input field
+        return false
       }
-      // Goal: Do not see C typed in the input field
-      return false
     },
     dotPhrases() {
       console.log('invoke dp')
