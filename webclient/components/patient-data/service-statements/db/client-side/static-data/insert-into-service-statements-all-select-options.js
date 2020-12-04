aa35b37da38bd8fc84749f52816a66740396b50e
Vikas <kedia.vikas@gmail.com>
2020-12-03 21:07:59 -0800

    +r

--- webclient/components/patient-data/service-statements/db/client-side/static-data/insert-into-service-statements-all-select-options.js
+++ webclient/components/patient-data/service-statements/db/client-side/static-data/insert-into-service-statements-all-select-options.js
@@ -109,24 +109,4 @@ const selectOptions = {
   ],
 }
 
-/*
-let serviceStatementsAllSelectOptionsTbl = new Array()
-
-let rowUniqId = 0
-let fldName = {}
-for (fldName in selectOptions) {
-  const obj = selectOptions[fldName]
-  for (let i = 0; i < obj.length; i++) {
-    const label = obj[i].label
-    serviceStatementsAllSelectOptionsTbl.push({
-      fieldOptionId: '#' + label.replace(/ /g, '_') + '#', // # is the seperator charecter so toggle can work. Look inside manage-rows
-      fieldOptionLabel: label,
-      fieldNameInDb: fldName,
-    })
-    rowUniqId++
-  }
-}
-*/
-exports.selectOptions = selectOptions
-
 exports.serviceStatementsAllSelectOptionsTbl = selectOptions
