345dc08369b328f889dbf6c44aaecaa7bad05751
Vikas <kedia.vikas@gmail.com>
2020-12-04 22:37:54 -0800

    edit of CC works from left tab bar

--- webclient/components/patient-data/chief-complaint/change-layer/edit-design-1.vue
+++ webclient/components/patient-data/chief-complaint/change-layer/edit-design-1.vue
@@ -1,25 +1,28 @@
 <template>
-  <ctEditStructure :_formDef="formDef" :firstProp="firstProp"></ctEditStructure>
+  <div
+    v-if="rowId"
+    why="This is inside v-if so the component does not get called if the rowId has not been fetched yet."
+  >
+    <ctEditStructure :_formDef="formDef" :firstProp="rowId"></ctEditStructure>
+  </div>
 </template>
 
 <script>
 import ctEditStructure from '@/components//papers/note-change/templates/edit-form.vue'
 import { chiefComplaintFormDef } from '@/components/patient-data/chief-complaint/db/client-side/structure/chief-complaint-of-a-patient-table.js'
+import chiefComplainTbl from '@/components/patient-data/chief-complaint/db/client-side/structure/chief-complaint-of-a-patient-table.js'
 
 export default {
   data: function () {
     return {
       formDef: chiefComplaintFormDef,
+      rowId: null,
     }
-  },
-  props: {
-    firstProp: {
-      type: Number,
-    },
   },
 
   mounted() {
-    //    console.log(this.dataFormFields)
+    const status = chiefComplainTbl.fnGetPresentUniqueUuidNotEmptyRows(['description'])
+    this.rowId = status[0]['clientSideUniqRowId']
   },
 
   components: {
