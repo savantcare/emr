4b511938f45bf8fe529b63733bf5a4eeb93eafb4
Vikas <kedia.vikas@gmail.com>
2020-12-07 09:30:23 -0800

    +r

--- webclient/components/patient-data/chief-complaint/change-layer/add-chief-complaint.vue
+++ webclient/components/patient-data/chief-complaint/change-layer/add-chief-complaint.vue
@@ -22,8 +22,16 @@ export default {
       options: {
         autocompleteMode: true,
         values: [
-          { key: 'depression', value: 'Patient has history of depression' },
-          { key: 'anxiety', value: 'Patient has history of anxiety' },
+          { key: 'depression - Patient has history of depression', value: 'Patient has history of depression' },
+          { key: 'anxiety - Patient has history of anxiety', value: 'Patient has history of anxiety' },
+          { key: 'past - Past substance use', value: 'Past substance use' },
+          { key: 'ex', value: 'examination' },
+          { key: 'hx', value: 'history' },
+          {
+            key: 'mdm low risk',
+            value:
+              'Patoient is deemed low risk becaquse patient has 2 chromnic iolleness and taking prescription medication',
+          },
         ],
         positionMenu: false,
         menuContainer: document.body,
