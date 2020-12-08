82e0ea4b51658476ad27adae0b2d81bb09d906e2
Vikas <kedia.vikas@gmail.com>
2020-12-08 03:17:27 -0800

    +r

--- webclient/components/patient-data/medication-list/change-layer/add.vue
+++ webclient/components/patient-data/medication-list/change-layer/add.vue
@@ -1,5 +1,79 @@
 <template>
-  <div>Medication list</div>
+  <div>
+    Medication list
+
+    <ag-grid-vue
+      style="width: 800px; height: 500px"
+      class="ag-theme-alpine"
+      :columnDefs="columnDefs"
+      :rowData="rowData"
+    >
+    </ag-grid-vue>
+  </div>
 </template>
 
-<script></script>
+<script>
+import { AgGridVue } from 'ag-grid-vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      columnDefs: null,
+      rowData: null,
+    }
+  },
+  components: {
+    AgGridVue,
+  },
+  beforeMount() {
+    this.columnDefs = [
+      { headerName: 'Name', field: 'name' },
+      { headerName: 'Dosage', field: 'dosage' },
+      { headerName: 'Instructions', field: 'instructions' },
+      { headerName: 'Start date', field: 'startDate' },
+      { headerName: 'End date', field: 'endDate' },
+      { headerName: 'Reconciled on', field: 'reconciledOn' },
+      { headerName: 'Connected orders', field: 'connectedOrders' },
+      { headerName: 'Notes', field: 'notes' },
+    ]
+
+    this.rowData = [
+      {
+        name: 'Lexapro',
+        dosage: '1',
+        instructions: 'daily',
+        startDate: 'a',
+        endDate: 'b',
+        reconciledOn: 'a',
+        connectedOrders: 'a',
+        notes: 's',
+      },
+      {
+        name: 'Prozac',
+        dosage: '2',
+        instructions: 'weekly',
+        startDate: 'a',
+        endDate: 'b',
+        reconciledOn: 'a',
+        connectedOrders: 'a',
+        notes: 's',
+      },
+      {
+        name: 'Aspirin',
+        dosage: '3',
+        instructions: 'as needed',
+        startDate: 'a',
+        endDate: 'b',
+        reconciledOn: 'a',
+        connectedOrders: 'a',
+        notes: 's',
+      },
+    ]
+  },
+}
+</script>
+<style lang="scss">
+@import 'ag-grid-community/dist/styles/ag-grid.css';
+@import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+</style>
