6db430ff4aca7c83a973e7bab0fa8e00c89501fd
Vikas <kedia.vikas@gmail.com>
2020-12-11 03:37:10 -0800

    +r

--- webclient/components/non-temporal/tribute/patient-data-dynamic-collection-terms-start-with-equal-to.js
+++ webclient/components/non-temporal/tribute/patient-data-dynamic-collection-terms-start-with-equal-to.js
@@ -1,16 +1,26 @@
-tributeOptions: {
-    autocompleteMode: true,
-    values: [
-      { key: '=dob', value: 'Patient has history of depression' },
-      { key: '=name', value: 'Patient has history of anxiety' },
-      { key: '=dxlist', value: 'Past substance use' },
-      { key: '=medlist', value: 'examination' },
-      { key: '=recommendations', value: 'history' },
+const patientDataPoints = 
+[
+      { key: '=dob', value: '' },
+      { key: '=name', value: '' },
+      { key: '=dxlist', value: '' },
+      { key: '=medlist', value: '' },
+      { key: '=recommendations', value: '' },
       {
         key: '=reminders',
         value:
-          'Patoient is deemed low risk becaquse patient has 2 chromnic iolleness and taking prescription medication',
+          '',
       },
-    ],
-  
+    ]
+    
 
+  for(keys in patientDataPoints)
+
+  if(key ==="=dob") {
+    patientDataPoints.value = vuex.orm.query(get DOB)
+  }
+
+  if(key ==="=name") {
+    patientDataPoints.value = vuex.orm.query(get name)
+  }
+
+export patientDataPoints
