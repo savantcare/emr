424fefdcbbaea844d9e984092b3cf50e5c3d0c66
Raj Roushan Jha <raj@grmtech.com>
2020-12-15 13:09:22 +0530

    implement merged collection terms in tribute

--- webclient/components/non-temporal/tribute/diagnosis-master-collection.js
+++ webclient/components/non-temporal/tribute/diagnosis-master-collection.js
@@ -1,1 +1,10 @@
 // this will have list of all the dx. This is not patient specifig.
+const diagnosisMasterDataPoints = [
+  { key: 'ADHD', value: 'ADHD' },
+  { key: 'Depression', value: 'Depression' },
+  { key: 'Anxiety', value: 'Anxiety' },
+  { key: 'Flu', value: 'Flu' },
+  { key: 'Pain', value: 'Pain' },
+]
+
+export default diagnosisMasterDataPoints
--- webclient/components/patient-data/chief-complaint/db/client-side/structure/chief-complaint-of-a-patient-table.js
+++ webclient/components/patient-data/chief-complaint/db/client-side/structure/chief-complaint-of-a-patient-table.js
@@ -62,27 +62,6 @@ export const chiefComplaintFormDef = {
       showLabel: false,
       type: 'tribute-input',
       showHistory: 'always',
-      tributeOptions: {
-        autocompleteMode: true,
-        values: [
-          { key: 'depression - Patient has history of depression', value: 'Patient has history of depression' },
-          { key: 'anxiety - Patient has history of anxiety', value: 'Patient has history of anxiety' },
-          { key: 'past - Past substance use', value: 'Past substance use' },
-          { key: 'ex', value: 'examination' },
-          { key: 'hx', value: 'history' },
-          {
-            key: 'mdm low risk',
-            value:
-              'Patoient is deemed low risk becaquse patient has 2 chromnic iolleness and taking prescription medication',
-          },
-        ],
-        positionMenu: true,
-        menuContainer: document.querySelector('.menu-container'),
-        noMatchTemplate: '',
-        //selectTemplate: function (item) {
-        //return '@' + item.original.value
-        //},
-      },
     },
   ],
   showReviewedButtonInForm: false,
