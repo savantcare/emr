85244dc4c65a8b357f733a2f19a994e6b55ebee3
ss <ss@166desktop.local>
2021-02-01 19:58:32 +0530

    MROS-when-none-is-selected-others-need-to-removed-from-vuex-orm

--- webclient/components/non-temporal/form-manager/manage-rows-of-table-in-client-side-orm.js
+++ webclient/components/non-temporal/form-manager/manage-rows-of-table-in-client-side-orm.js
@@ -643,6 +643,11 @@ Decision: We will make arOrmRowsCached as a 3D array. Where the 1st D will be en
         valueForThisField = null
       }
 
+      // Goal: when user select option "None" remove old selected values from vuex-orm
+      if(valueForThisField && valueForThisField.includes('#None#')){
+        valueForThisField = '#None#'
+      }
+
       row = {
         [pFldName]: valueForThisField,
         vnRowStateInSession: pRowStatus,
