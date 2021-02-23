3f732951a947c318e1053cc3cd577616935b9049
srd <sreeparna@grmtech.com>
2021-02-23 18:56:02 +0530

    updated for family history add edit delete

---
+++ servers/lumen/app/FamilyHistory.php
@@ -0,0 +1,40 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
+
+class FamilyHistory extends Model
+{
+    protected $connection = 'familyHistoryDB';
+    protected $table = 'family_history';
+    public $timestamps = false;
+    protected $primaryKey = 'serverSideRowUuid';
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'ptUuid', 'description', 'relationship', 'recordChangedByUuid',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function (Model $model) {
+            $model->setAttribute($model->getKeyName(), Uuid::uuid4());
+        });
+    }
+}
