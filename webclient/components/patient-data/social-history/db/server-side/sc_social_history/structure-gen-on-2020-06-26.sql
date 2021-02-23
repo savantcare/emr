54810cb0d64ce28f2ce3451af9749417258bc14c
abhilash-grmtech <abhilash@grmtech.com>
2021-02-23 20:01:30 +0530

    complete social history component

--- webclient/components/patient-data/social-history/db/server-side/sc_social_history/structure-gen-on-2020-06-26.sql
+++ webclient/components/patient-data/social-history/db/server-side/sc_social_history/structure-gen-on-2020-06-26.sql
@@ -5,7 +5,11 @@ DROP TABLE IF EXISTS `social_history`;
 CREATE TABLE `social_history` (
   `serverSideRowUuid` char(36) NOT NULL,
   `ptUuid` char(36) NOT NULL,
-  `description` text NOT NULL,
+  `current_social_situation` text NOT NULL,
+  `education_work_history` text NOT NULL,
+  `social_supports` text NOT NULL,
+  `developmental_history` text NOT NULL,
+  `trauma_history` text NOT NULL,
   `recordChangedByUuid` char(36) NOT NULL,
   `recordChangedFromIPAddress` varchar(20) NOT NULL,
   `recordChangedFromSection` varchar(255) NOT NULL DEFAULT 'patientFile',
